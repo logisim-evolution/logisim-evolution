e97e8ede543e779a852ce12f6f5bc61d66c17dfa
Roberto Rigamonti <roberto.rigamonti@heig-vd.ch>
2014-12-16 15:43:27 +0100

    Fixed bin directory

--- eclipse_fix.sh
+++ eclipse_fix.sh
@@ -3,8 +3,10 @@
 
 #!/bin/bash
 
-ln -s ${PWD}/boards_model ${PWD}/bin/boards_model
-ln -s ${PWD}/javax ${PWD}/bin/javax
-ln -s ${PWD}/libs ${PWD}/bin/libs
-ln -s ${PWD}/resources ${PWD}/bin/resources
-ln -s ${PWD}/doc ${PWD}/bin/doc
+mkdir -p bin
+cd bin
+ln -s ../boards_model .
+ln -s ../javax .
+ln -s ../libs .
+ln -s ../resources .
+ln -s ../doc .
