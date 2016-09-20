217e3f0fb09e84d1e1d2bb355ed3aa6e883cce38
Alexandre Malki <alexandre.malki@heig-vd.ch>
2016-09-20 08:18:34 +0200

    Stable version 2.13.20
    Conflicts:
    	src/com/cburch/logisim/Main.java

--- src/com/cburch/logisim/Main.java
+++ src/com/cburch/logisim/Main.java
@@ -65,8 +65,9 @@ public class Main {
 
 	final static Logger logger = LoggerFactory.getLogger(Main.class);
 
-	public static final LogisimVersion VERSION = LogisimVersion.get(2, 13, 19,
+	public static final LogisimVersion VERSION = LogisimVersion.get(2, 13, 20,
 			LogisimVersion.FINAL_REVISION);
+
 	public static final String VERSION_NAME = VERSION.toString();
 	public static final int COPYRIGHT_YEAR = 2014;
 
