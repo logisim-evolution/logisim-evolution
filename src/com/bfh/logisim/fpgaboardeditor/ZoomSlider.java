aa817accf5ee33917ed4fcaebbe796e38e7eb51c
Kluter Theo <theo.kluter@bfh.ch>
2017-03-20 08:35:14 +0100

    Call to revalidate the components, maybe this helps on MAC.

--- src/com/bfh/logisim/fpgaboardeditor/ZoomSlider.java
+++ src/com/bfh/logisim/fpgaboardeditor/ZoomSlider.java
@@ -26,10 +26,13 @@ public class ZoomSlider extends JSlider {
 		 setPaintTicks(true);
 		 Hashtable<Integer, JLabel> labelTable = new Hashtable<Integer, JLabel>();
 		 label = new JLabel("1.0x");
+		 label.setFont(AppPreferences.getScaledFont(label.getFont()));
 		 labelTable.put(new Integer(100), label);
 		 label = new JLabel("1.5x");
+		 label.setFont(AppPreferences.getScaledFont(label.getFont()));
 		 labelTable.put(new Integer(150), label);
 		 label = new JLabel("2.0x");
+		 label.setFont(AppPreferences.getScaledFont(label.getFont()));
 		 labelTable.put(new Integer(200), label);
 		 setLabelTable(labelTable);
 		 setPaintLabels(true);
