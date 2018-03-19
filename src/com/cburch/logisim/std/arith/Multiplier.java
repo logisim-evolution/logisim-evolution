cf8f55af568e039150187c3dd64e55b851875042
Leijurv <leijurv@gmail.com>
2018-03-19 12:51:52 -0700

    Added missing import

--- src/com/cburch/logisim/std/arith/Multiplier.java
+++ src/com/cburch/logisim/std/arith/Multiplier.java
@@ -45,6 +45,7 @@ import com.cburch.logisim.instance.InstanceFactory;
 import com.cburch.logisim.instance.InstancePainter;
 import com.cburch.logisim.instance.InstanceState;
 import com.cburch.logisim.instance.Port;
+import com.cburch.logisim.instance.StdAttr;
 import com.cburch.logisim.tools.key.BitWidthConfigurator;
 import com.cburch.logisim.util.GraphicsUtil;
 
