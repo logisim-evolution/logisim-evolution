e184020533bf9d996426745049e11bfae0da6881
Dr.Ir.Ing. Theo Kluter <ktt1@bfh.ch>
2019-05-28 16:15:19 +0200

    Removed imogies and replaced by scalable icons

---
+++ src/main/java/com/cburch/logisim/gui/icons/QuestionIcon.java
@@ -0,0 +1,75 @@
+/**
+ * This file is part of logisim-evolution.
+ *
+ * Logisim-evolution is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Logisim-evolution is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with logisim-evolution. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Original code by Carl Burch (http://www.cburch.com), 2011.
+ * Subsequent modifications by:
+ *   + College of the Holy Cross
+ *     http://www.holycross.edu
+ *   + Haute École Spécialisée Bernoise/Berner Fachhochschule
+ *     http://www.bfh.ch
+ *   + Haute École du paysage, d'ingénierie et d'architecture de Genève
+ *     http://hepia.hesge.ch/
+ *   + Haute École d'Ingénierie et de Gestion du Canton de Vaud
+ *     http://www.heig-vd.ch/
+ */
+
+package com.cburch.logisim.gui.icons;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.font.TextLayout;
+
+import javax.swing.Icon;
+
+import com.cburch.logisim.prefs.AppPreferences;
+
+public class QuestionIcon implements Icon {
+
+  int wh = AppPreferences.getIconSize();
+
+  @Override
+  public void paintIcon(Component c, Graphics g, int x, int y) {
+    Graphics2D g2 = (Graphics2D) g.create();
+    g2.translate(x, y);
+    g2.setColor(Color.GREEN);
+    g2.fillRect(0, 0, wh-1, wh-1);
+    g2.setColor(Color.BLACK);
+    g2.setStroke(new BasicStroke(AppPreferences.getScaled((float)1)));
+    g2.drawRect(0, 0, wh-1, wh-1);
+    g2.setColor(Color.GREEN.darker().darker().darker().darker());
+    Font f = g2.getFont().deriveFont((float)wh/(float)1.3).deriveFont(Font.BOLD);
+    TextLayout t = new TextLayout("?",f,g2.getFontRenderContext());
+    float xc = (float)wh/(float)2-(float)t.getBounds().getCenterX();
+    float yc = (float)wh/(float)2-(float)t.getBounds().getCenterY();
+    t.draw(g2, xc, yc);
+    g2.dispose();
+  }
+
+  @Override
+  public int getIconWidth() {
+    return wh;
+  }
+
+  @Override
+  public int getIconHeight() {
+    return wh;
+  }
+
+}
