1ac16e61e35c1b71ac369c211bdb008b4702daa2
David Hutchens <David.Hutchens@millersville.edu>
2021-01-08 21:10:56 -0500

    Remove old mac application build and upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,10 @@ import java.util.Date
 import java.text.SimpleDateFormat
 
 plugins {
-    id("com.github.ben-manes.versions") version "0.28.0"
+    id("com.github.ben-manes.versions") version "0.36.0"
     java
     application
-    id("com.github.johnrengelman.shadow") version "5.2.0"
-    id("edu.sc.seis.macAppBundle") version "2.3.0"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
 } 
 
 repositories {
@@ -161,52 +160,6 @@ tasks {
             include("LICENSE")
             include("README.md")
         }
-    }
-    macAppBundle {
-        mainClassName = "com.cburch.logisim.Main"
-        runtimeConfigurationName = "shadow"
-        jarTask = "shadowJar"
-        bundleJRE = false
-        highResolutionCapable = true
-        appName = "Logisim-evolution"
-        appStyle = "universalJavaApplicationStub"
-        bundleIdentifier = "com.cburch.logisim"
-        creatorCode = "????"
-        icon = "src/main/resources/resources/logisim/img/Logisim-evolution.icns"
-        // backgroundImage = "src/main/resources/resources/logisim/img/logisim-icon-128.png"
-        // javaProperties.put("apple.laf.useScreenMenuBar", "true")
-        bundleExtras.put("CFBundleDisplayName", "Logisim-evolution")
-        bundleExtras.put(
-            "CFBundleDocumentTypes",
-            arrayOf(
-                mapOf(
-                    "LSItemContentTypes" to arrayOf("com.cburch.logisim.circ"),
-                    "CFBundleTypeName" to "Logisim-evolution circuit file",
-                    "LSHandlerRank" to "Owner",
-                    "CFBundleTypeRole" to "Editor",
-                    "LSIsAppleDefaultForType" to true
-                )
-            )
-        )
-        bundleExtras.put(
-            "UTExportedTypeDeclarations",
-            arrayOf(
-                mapOf(
-                    "UTTypeIdentifier" to "com.cburch.logisim.circ",
-                    "UTTypeDescription" to "Logisim-evolution circuit file",
-                    "UTTypeConformsTo" to arrayOf("public.data"),
-                    "UTTypeIconFile" to "Logisim-evolution.icns",
-                    "UTTypeTagSpecification" to
-                    mapOf(
-                        "public.filename-extension" to arrayOf("circ"),
-                        "public.mime-type" to arrayOf("application-prs.cburch.logisim")
-                    )
-                )
-            )
-        )
-        bundleExtras.put("LSApplicationCategoryType", "public.app-category.education")
-        bundleExtras.put("NSHumanReadableCopyright", "Copyright © 2001–2020 Carl Burch, BFH, HEIG-VD, HEPIA, Holy Cross, et al.")
-        bundleExtras.put("NSSupportsAutomaticGraphicsSwitching", "true")
     }
 }
 
