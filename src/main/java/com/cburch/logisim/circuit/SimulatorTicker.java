0042ec2b3eb9f26f8cc0f337a1b69be3053fe908
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2021-01-31 16:36:09 +0100

    Remove double asterisk from copyright notice

--- src/main/java/com/cburch/logisim/circuit/SimulatorTicker.java
+++ src/main/java/com/cburch/logisim/circuit/SimulatorTicker.java
@@ -1,4 +1,4 @@
-/**
+/*
  * This file is part of logisim-evolution.
  *
  * <p>Logisim-evolution is free software: you can redistribute it and/or modify it under the terms
--- src/main/java/com/cburch/logisim/soc/rv32im/RV32imControlTransferInstructions.java
+++ src/main/java/com/cburch/logisim/soc/rv32im/RV32imControlTransferInstructions.java
@@ -1,4 +1,4 @@
-/**
+/*
  * This file is part of logisim-evolution.
  *
  * Logisim-evolution is free software: you can redistribute it and/or modify
