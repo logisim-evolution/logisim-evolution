0922135198e91a5be362ed6468bbea6f1880f1fa
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2021-02-02 14:42:52 +0100

    Upgrade to Java 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,8 +41,8 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_9
-    targetCompatibility = JavaVersion.VERSION_1_9
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 task<Jar>("sourcesJar") {
