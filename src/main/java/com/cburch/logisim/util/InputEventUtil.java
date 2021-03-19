13faf3b26b51663671f2d4aa54c640d3f718229e
Torsten Maehne <torsten.maehne@bfh.ch>
2021-03-19 22:40:52 +0100

    Use class InputEvent and associated new masks instead of deprecated class Event

--- src/main/java/com/cburch/logisim/util/InputEventUtil.java
+++ src/main/java/com/cburch/logisim/util/InputEventUtil.java
@@ -96,10 +96,10 @@ public class InputEventUtil {
 
   public static String toKeyDisplayString(int mods) {
     ArrayList<String> arr = new ArrayList<>();
-    if ((mods & Event.META_MASK) != 0) arr.add(S.get("metaMod"));
-    if ((mods & Event.CTRL_MASK) != 0) arr.add(S.get("ctrlMod"));
-    if ((mods & Event.ALT_MASK) != 0) arr.add(S.get("altMod"));
-    if ((mods & Event.SHIFT_MASK) != 0) arr.add(S.get("shiftMod"));
+    if ((mods & InputEvent.META_DOWN_MASK) != 0) arr.add(S.get("metaMod"));
+    if ((mods & InputEvent.CTRL_DOWN_MASK) != 0) arr.add(S.get("ctrlMod"));
+    if ((mods & InputEvent.ALT_DOWN_MASK) != 0) arr.add(S.get("altMod"));
+    if ((mods & InputEvent.SHIFT_DOWN_MASK) != 0) arr.add(S.get("shiftMod"));
 
     Iterator<String> it = arr.iterator();
     if (it.hasNext()) {
