e20a54a5f5e6557a2dffa16c6ea457df12b3d2be
Torsten Maehne <torsten.maehne@bfh.ch>
2021-05-20 08:36:05 +0200

    Enable fall-through warnings

--- build.gradle.kts
+++ build.gradle.kts
@@ -177,10 +177,10 @@ tasks.register("jpackage") {
 
 tasks {
     compileJava {
-        options.compilerArgs = listOf("-Xlint:deprecation", "-Xlint:unchecked")
+        options.compilerArgs = listOf("-Xlint:deprecation", "-Xlint:unchecked", "-Xlint:fallthrough")
     }
     compileTestJava {
-        options.compilerArgs = listOf("-Xlint:deprecation", "-Xlint:unchecked")
+        options.compilerArgs = listOf("-Xlint:deprecation", "-Xlint:unchecked", "-Xlint:fallthrough")
     }
     jar {
         manifest {
