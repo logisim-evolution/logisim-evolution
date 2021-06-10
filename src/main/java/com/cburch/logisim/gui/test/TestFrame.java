56537a467c10363af4580a64a249ac28af2133c2
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2021-06-10 20:32:27 +0200

    ESC closes "Test Vector" frame now

--- src/main/java/com/cburch/logisim/gui/test/TestFrame.java
+++ src/main/java/com/cburch/logisim/gui/test/TestFrame.java
@@ -48,15 +48,12 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class TestFrame extends LFrame.SubWindowWithSimulation {
 
@@ -119,6 +116,11 @@ public class TestFrame extends LFrame.SubWindowWithSimulation {
     LocaleManager.addLocaleListener(myListener);
     myListener.localeChanged();
     pack();
+
+    // Close the frame once ESC is hit
+    getRootPane().registerKeyboardAction(al -> dispose(),
+            KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+            JComponent.WHEN_IN_FOCUSED_WINDOW);
   }
 
   private static String computeTitle(Model data, Project proj) {
