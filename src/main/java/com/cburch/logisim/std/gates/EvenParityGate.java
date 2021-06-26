542b8d54c959e3059546c94aa871edeaf6cd9ad7
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2021-06-26 14:06:31 +0200

    Reduced code verbosity with `final var`/`var`

--- src/main/java/com/cburch/logisim/std/gates/EvenParityGate.java
+++ src/main/java/com/cburch/logisim/std/gates/EvenParityGate.java
@@ -42,16 +42,16 @@ import java.util.ArrayList;
 class EvenParityGate extends AbstractGate {
   private static class XNorGateHDLGeneratorFactory extends AbstractGateHDLGenerator {
     @Override
-    public ArrayList<String> GetLogicFunction(int nr_of_inputs, int bitwidth, boolean is_one_hot) {
-      ArrayList<String> Contents = new ArrayList<>();
-      Contents.addAll(GetParity(true, nr_of_inputs, bitwidth > 1));
-      Contents.add("");
-      return Contents;
+    public ArrayList<String> GetLogicFunction(int nrOfInputs, int bitwidth, boolean isOneHot) {
+      final var contents = new ArrayList<String>();
+      contents.addAll(GetParity(true, nrOfInputs, bitwidth > 1));
+      contents.add("");
+      return contents;
     }
   }
 
   public static final EvenParityGate FACTORY = new EvenParityGate();
-  private final String LABEL = "2k";
+  private static final String LABEL = "2k";
 
   private EvenParityGate() {
     super("Even Parity", S.getter("evenParityComponent"));
@@ -60,8 +60,8 @@ class EvenParityGate extends AbstractGate {
 
   @Override
   protected Expression computeExpression(Expression[] inputs, int numInputs) {
-    Expression ret = inputs[0];
-    for (int i = 1; i < numInputs; i++) {
+    var ret = inputs[0];
+    for (var i = 1; i < numInputs; i++) {
       ret = Expressions.xor(ret, inputs[i]);
     }
     return Expressions.not(ret);
--- src/main/java/com/cburch/logisim/std/gates/PLAHDLGeneratorFactory.java
+++ src/main/java/com/cburch/logisim/std/gates/PLAHDLGeneratorFactory.java
@@ -33,7 +33,6 @@ import com.cburch.logisim.fpga.designrulecheck.Netlist;
 import com.cburch.logisim.fpga.designrulecheck.NetlistComponent;
 import com.cburch.logisim.fpga.hdlgenerator.AbstractHDLGeneratorFactory;
 import com.cburch.logisim.fpga.hdlgenerator.HDL;
-
 import java.util.ArrayList;
 import java.util.SortedMap;
 import java.util.TreeMap;
@@ -46,65 +45,65 @@ public class PLAHDLGeneratorFactory extends AbstractHDLGeneratorFactory {
   }
 
   @Override
-  public SortedMap<String, Integer> GetInputList(Netlist TheNetlist, AttributeSet attrs) {
-    SortedMap<String, Integer> Inputs = new TreeMap<>();
-    Inputs.put("Index", attrs.getValue(PLA.ATTR_IN_WIDTH).getWidth());
-    return Inputs;
+  public SortedMap<String, Integer> GetInputList(Netlist nets, AttributeSet attrs) {
+    SortedMap<String, Integer> inputs = new TreeMap<>();
+    inputs.put("Index", attrs.getValue(PLA.ATTR_IN_WIDTH).getWidth());
+    return inputs;
   }
 
   private static String bits(char[] b) {
-    StringBuilder s = new StringBuilder();
-    for (char c : b) s.insert(0, ((c == '0' || c == '1') ? c : '-'));
+    final var s = new StringBuilder();
+    for (final var c : b) s.insert(0, ((c == '0' || c == '1') ? c : '-'));
     if (b.length == 1) return "'" + s + "'";
     else return "\"" + s + "\"";
   }
 
   private static String zeros(int sz) {
-    StringBuilder s = new StringBuilder();
+    final var s = new StringBuilder();
     s.append("0".repeat(sz));
     if (sz == 1) return "'" + s + "'";
     else return "\"" + s + "\"";
   }
 
   @Override
-  public ArrayList<String> GetModuleFunctionality(Netlist TheNetlist, AttributeSet attrs) {
-    ArrayList<String> Contents = new ArrayList<>();
+  public ArrayList<String> GetModuleFunctionality(Netlist nets, AttributeSet attrs) {
+    final var contents = new ArrayList<String>();
     PLATable tt = attrs.getValue(PLA.ATTR_TABLE);
     int outSz = attrs.getValue(PLA.ATTR_OUT_WIDTH).getWidth();
     if (HDL.isVHDL()) {
-      String leader = "    Result <= ";
-      String indent = "              ";
+      var leader = "    Result <= ";
+      final var indent = "              ";
       if (tt.rows().isEmpty()) {
-        Contents.add(leader + zeros(outSz) + ";");
+        contents.add(leader + zeros(outSz) + ";");
       } else {
         for (PLATable.Row r : tt.rows()) {
-          Contents.add(
+          contents.add(
               leader + bits(r.outBits) + " WHEN std_match(Index, " + bits(r.inBits) + ") ELSE");
           leader = indent;
         }
-        Contents.add(leader + zeros(outSz) + ";");
+        contents.add(leader + zeros(outSz) + ";");
       }
     } else {
       // todo
     }
-    return Contents;
+    return contents;
   }
 
   @Override
-  public SortedMap<String, Integer> GetOutputList(Netlist TheNetlist, AttributeSet attrs) {
-    SortedMap<String, Integer> Outputs = new TreeMap<>();
-    Outputs.put("Result", attrs.getValue(PLA.ATTR_OUT_WIDTH).getWidth());
-    return Outputs;
+  public SortedMap<String, Integer> GetOutputList(Netlist nets, AttributeSet attrs) {
+    SortedMap<String, Integer> outputs = new TreeMap<>();
+    outputs.put("Result", attrs.getValue(PLA.ATTR_OUT_WIDTH).getWidth());
+    return outputs;
   }
 
   @Override
-  public SortedMap<String, String> GetPortMap(Netlist Nets, Object MapInfo) {
-    SortedMap<String, String> PortMap = new TreeMap<>();
-    if (!(MapInfo instanceof NetlistComponent)) return PortMap;
-    NetlistComponent ComponentInfo = (NetlistComponent) MapInfo;
-    PortMap.putAll(GetNetMap("Index", true, ComponentInfo, PLA.IN_PORT, Nets));
-    PortMap.putAll(GetNetMap("Result", true, ComponentInfo, PLA.OUT_PORT, Nets));
-    return PortMap;
+  public SortedMap<String, String> GetPortMap(Netlist nets, Object mapInfo) {
+    SortedMap<String, String> portMap = new TreeMap<>();
+    if (!(mapInfo instanceof NetlistComponent)) return portMap;
+    final var componentInfo = (NetlistComponent) mapInfo;
+    portMap.putAll(GetNetMap("Index", true, componentInfo, PLA.IN_PORT, nets));
+    portMap.putAll(GetNetMap("Result", true, componentInfo, PLA.OUT_PORT, nets));
+    return portMap;
   }
 
   @Override
