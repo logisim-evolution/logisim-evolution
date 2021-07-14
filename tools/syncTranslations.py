05fab32242c7147e10e37f894db900ccac42c70c
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2021-07-14 14:01:34 +0200

    Reworked report to be more compact

--- tools/syncTranslations.py
+++ tools/syncTranslations.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import argparse
+import os
+
 def getKeys(path):
     keys = []
     with open(path, 'r', encoding="utf8") as file:
@@ -20,9 +23,7 @@ def getTrans(path):
     with open(path, 'r', encoding="utf8") as file:
         lines = file.readlines()
         for line in lines:
-            if line == '':
-                continue
-            if line[0] == '#':
+            if line == '' or line[0] == '#':
                 continue
             words = line.split('=')
             if len(words) <= 1:
@@ -31,9 +32,9 @@ def getTrans(path):
     return trans
 
 
-def writeFile(path, keys, trans):
+def writeFile(args, path, keys, trans):
     lines = []
-    c = 0       # number of missing translations
+    missingTranslations = 0
     for key in keys:
         if key in trans:
             if trans[key] != '':
@@ -43,25 +44,59 @@ def writeFile(path, keys, trans):
             lines.append(key + '\n')
         else:
             lines.append("# ==> " + key + " =\n")
-            c += 1
+            missingTranslations += 1
     lines = ''.join(lines) + '\n\n'
-    with open(path, 'w', encoding="utf8") as file:
-        file.write(lines)
-    return c
-        
-        
-showMissing = False     # output number of missing translations
-path = "../src/main/resources/resources/logisim/strings/"
-files = ("analyze","circuit", "data", "draw", "file", "fpga", "gui", "hdl", "proj", "soc", "std", "tools", "util")
-langs = ("de", "el", "es", "fr", "pt", "ru", "it", "nl", "ja", "pl")
+    if (args.write):
+        with open(path, 'w', encoding="utf8") as file:
+            file.write(lines)
+    return missingTranslations
 
-for file in files:
-    filePath = path + file +'/'+file
-    if showMissing:
-        print(file + ':')
-    keys = getKeys(filePath + ".properties")
-    for lang in langs:
-        trans = getTrans(filePath+'_'+lang+".properties")
-        missing = writeFile(filePath+'_'+lang+".properties", keys, trans)
-        if showMissing and missing > 0:
-            print("  " + lang + ": " + str(missing))
+
+def main():
+    defaultRootDir = "../src/main/resources/resources/logisim/strings/"
+    files = sorted(["analyze", "circuit", "data", "draw", "file", "fpga", "gui", "hdl", "proj", "soc", "std", "tools", "util"])
+    maxFileNameLen = max([len(i) for i in files]) + 2
+    langs = sorted(["de", "el", "es", "fr", "pl", "pt", "ru", "it", "nl", "ja"])
+
+    parser = argparse.ArgumentParser()
+    group = parser.add_argument_group('Options')
+    group.add_argument('--write', action = 'store_true', dest = 'write',
+                       help = "Update translation files")
+    group.add_argument('-q', '--quiet', action = 'store_true', dest = 'quiet')
+
+    group = parser.add_argument_group('Paths')
+    group.add_argument('--root', action = 'store', dest = 'rootDir', nargs = 1, metavar = 'DIR',
+                       help = 'String resources root dir. Default: {}'.format(defaultRootDir))
+
+    args = parser.parse_args()
+
+    if args.rootDir is None:
+        args.rootDir = defaultRootDir
+
+    if not args.quiet:
+        h1 = ' ' * maxFileNameLen + '|'
+        h2 = '-' * maxFileNameLen + '+'
+        for lang in langs:
+            h1 += '{:^5} |'.format(lang.upper())
+        h2 += '------+' * len(langs)
+        print('{}\n{}'.format(h1, h2))
+
+    for file in files:
+        filePath = os.path.join(args.rootDir, file, file)
+        if not args.quiet:
+            fmt = '{:<' + str(maxFileNameLen) + '}|'
+            print(fmt.format(file), end='')
+        keys = getKeys(filePath + ".properties")
+        for lang in langs:
+            trans = getTrans(filePath+'_'+lang+".properties")
+            missing = writeFile(args, filePath+'_'+lang+".properties", keys, trans)
+            if not args.quiet:
+                if missing == 0:
+                    missing = '-'
+                print(' {:>4} |'.format(missing), end = '')
+        if not args.quiet:
+            print()
+
+
+if __name__ == "__main__":
+    main()
