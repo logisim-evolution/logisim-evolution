afc9a38d8bd57809704d162eb0d4462a7bc0374f
Dr.Ir.Ing. Theo Kluter <ktt1@bfh.ch>
2021-08-25 20:49:12 +0200

    Adressed reviewers comments

--- src/main/java/com/cburch/logisim/std/ttl/Ttl74163.java
+++ src/main/java/com/cburch/logisim/std/ttl/Ttl74163.java
@@ -32,7 +32,12 @@ import com.cburch.logisim.instance.InstanceState;
 import com.cburch.logisim.instance.StdAttr;
 
 public class Ttl74163 extends Ttl74161 {
-
+  /**
+   * Unique identifier of the tool, used as reference in project files. Do NOT change as it will
+   * prevent project files from loading.
+   *
+   * <p>Identifier value must MUST be unique string among all tools.
+   */
   public static final String _ID = "74163";
 
   public Ttl74163() {
@@ -52,20 +57,13 @@ public class Ttl74163 extends Ttl74161 {
       if (nClear == 0) {
         counter = 0;
       } else if (nLoad == 0) {
-        counter = state.getPortValue(PORT_INDEX_A).toLongValue();
-        counter += state.getPortValue(PORT_INDEX_B).toLongValue() << 1;
-        counter += state.getPortValue(PORT_INDEX_C).toLongValue() << 2;
-        counter += state.getPortValue(PORT_INDEX_D).toLongValue() << 3;
+        counter = state.getPortValue(PORT_INDEX_A).toLongValue()
+            + state.getPortValue(PORT_INDEX_B).toLongValue() << 1
+            + state.getPortValue(PORT_INDEX_C).toLongValue() << 2
+            + state.getPortValue(PORT_INDEX_D).toLongValue() << 3;
       } else {
-        final var enpAndEnt = state.getPortValue(PORT_INDEX_EnP).and(state.getPortValue(PORT_INDEX_EnT)).toLongValue();
-        if (enpAndEnt == 1) {
-          counter++;
-          if (counter > 15) {
-            counter = 0;
-          }
-        } else {
-          return; // Nothing changed so return
-        }
+        if (state.getPortValue(PORT_INDEX_EnP).and(state.getPortValue(PORT_INDEX_EnT)).toLongValue() != 1) return; // Nothing changed so return 
+        counter = (counter + 1) & 15;
       }
       updateState(state, counter);
     } 
