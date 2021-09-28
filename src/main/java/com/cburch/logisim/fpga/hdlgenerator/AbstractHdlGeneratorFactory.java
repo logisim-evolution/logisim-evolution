0d2a13b3c1feb6832d2b00b6a9b93792b454258a
Prof. Dr. Theo Kluter <BFH-ktt1@users.noreply.github.com>
2021-09-28 22:09:28 +0200

    Update AbstractHdlGeneratorFactory.java

--- src/main/java/com/cburch/logisim/fpga/hdlgenerator/AbstractHdlGeneratorFactory.java
+++ src/main/java/com/cburch/logisim/fpga/hdlgenerator/AbstractHdlGeneratorFactory.java
@@ -24,6 +24,8 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
+import java.util.List;
+import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.TreeSet;
