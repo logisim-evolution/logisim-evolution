168d281ae591f171f3795e742b09a707f33392ad
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2021-10-16 21:32:50 +0200

    Code cleanup

--- src/main/java/com/cburch/hex/Test.java
+++ src/main/java/com/cburch/hex/Test.java
@@ -16,9 +16,9 @@ import javax.swing.JScrollPane;
 
 public class Test {
   public static void main(String[] args) {
-    JFrame frame = new JFrame();
-    HexModel model = new Model();
-    HexEditor editor = new HexEditor(model);
+    final var frame = new JFrame();
+    final var model = new Model();
+    final var editor = new HexEditor(model);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.getContentPane().add(new JScrollPane(editor));
     frame.pack();
@@ -29,53 +29,62 @@ public class Test {
     private final ArrayList<HexModelListener> listeners = new ArrayList<>();
     private final long[] data = new long[924];
 
+    @Override
     public void addHexModelListener(HexModelListener l) {
       listeners.add(l);
     }
 
+    @Override
     public void fill(long start, long len, long value) {
-      long[] oldValues = new long[(int) len];
+      final var oldValues = new long[(int) len];
       System.arraycopy(data, (int) (start - 11111), oldValues, 0, (int) len);
       Arrays.fill(data, (int) (start - 11111), (int) len, value);
-      for (HexModelListener l : listeners) {
-        l.bytesChanged(this, start, len, oldValues);
+      for (final var listener : listeners) {
+        listener.bytesChanged(this, start, len, oldValues);
       }
     }
 
+    @Override
     public long get(long address) {
       return data[(int) (address - 11111)];
     }
 
+    @Override
     public long getFirstOffset() {
       return 11111;
     }
 
+    @Override
     public long getLastOffset() {
       return data.length + 11110;
     }
 
+    @Override
     public int getValueWidth() {
       return 9;
     }
 
+    @Override
     public void removeHexModelListener(HexModelListener l) {
       listeners.remove(l);
     }
 
+    @Override
     public void set(long address, long value) {
-      long[] oldValues = new long[] {data[(int) (address - 11111)]};
+      final var oldValues = new long[] {data[(int) (address - 11111)]};
       data[(int) (address - 11111)] = value & 0x1FF;
-      for (HexModelListener l : listeners) {
-        l.bytesChanged(this, address, 1, oldValues);
+      for (final var listener : listeners) {
+        listener.bytesChanged(this, address, 1, oldValues);
       }
     }
 
+    @Override
     public void set(long start, long[] values) {
-      long[] oldValues = new long[values.length];
+      final var oldValues = new long[values.length];
       System.arraycopy(data, (int) (start - 11111), oldValues, 0, values.length);
       System.arraycopy(values, 0, data, (int) (start - 11111), values.length);
-      for (HexModelListener l : listeners) {
-        l.bytesChanged(this, start, values.length, oldValues);
+      for (final var listener : listeners) {
+        listener.bytesChanged(this, start, values.length, oldValues);
       }
     }
   }
