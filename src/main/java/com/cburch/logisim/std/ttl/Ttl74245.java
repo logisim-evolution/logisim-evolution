39182c1634a4c24d75904d706f7dae3e1bfc3c35
Bradley Bell <bradley.bell@gmail.com>
2022-06-26 16:46:30 -0700

    fix coding style

--- src/main/java/com/cburch/logisim/std/ttl/Ttl74245.java
+++ src/main/java/com/cburch/logisim/std/ttl/Ttl74245.java
@@ -30,14 +30,14 @@ public class Ttl74245 extends AbstractTtlGate {
 
   public Ttl74245() {
     super(
-      _ID,
-      (byte) 20,
-      new byte[] { 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18 },
-      new String[] {
-        "DIR", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8",
-        "B8", "B7", "B6", "B5", "B4", "B3", "B2", "B1", "nOE",
-      },
-      null);
+        _ID,
+        (byte) 20,
+        new byte[] { 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18 },
+        new String[] {
+          "DIR", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8",
+          "B8", "B7", "B6", "B5", "B4", "B3", "B2", "B1", "nOE",
+        },
+        null);
   }
 
   @Override
@@ -93,83 +93,83 @@ public class Ttl74245 extends AbstractTtlGate {
   private void drawBuffers(Graphics g, int x, int y, int height) {
     // DIR
     g.drawPolyline(
-      new int[] { x + 10, x + 10, x + 17, x + 17 },
-      new int[] { y + height - AbstractTtlGate.PIN_HEIGHT, y + 10, y + 10, y + 13 },
-      4);
+        new int[] { x + 10, x + 10, x + 17, x + 17 },
+        new int[] { y + height - AbstractTtlGate.PIN_HEIGHT, y + 10, y + 10, y + 13 },
+        4);
     g.drawOval(x + 16, y + 13, 2, 2);
 
     g.fillOval(x + 9, y + height - 11, 2, 2);
     g.drawPolyline(
-      new int[] { x + 10, x + 17, x + 17},
-      new int[] { y + height - 10, y + height - 10, y + height - 15 },
-      3);
+        new int[] { x + 10, x + 17, x + 17},
+        new int[] { y + height - 10, y + height - 10, y + height - 15 },
+        3);
 
     // nOE
     g.drawPolyline(
-      new int[] { x + 30, x + 30, x + 27, x + 23, x + 23 },
-      new int[] { y + AbstractTtlGate.PIN_HEIGHT, y + height - 13, y + height - 10, y + height - 10, y + height - 13 },
-      5);
+        new int[] { x + 30, x + 30, x + 27, x + 23, x + 23 },
+        new int[] { y + AbstractTtlGate.PIN_HEIGHT, y + height - 13, y + height - 10, y + height - 10, y + height - 13 },
+        5);
     g.drawOval(x + 22, y + height - 15, 2, 2);
 
     g.fillOval(x + 29, y + 9, 2, 2);
     g.drawPolyline(
-      new int[] { x + 30, x + 23, x + 23},
-      new int[] { y + 10, y + 10, y + 13 },
-      3);
+        new int[] { x + 30, x + 23, x + 23},
+        new int[] { y + 10, y + 10, y + 13 },
+        3);
     g.drawOval(x + 22, y + 13, 2, 2);
 
     // A enable
     g.drawPolyline(
-      new int[] { x + 15, x + 15, x + 25, x + 25 },
-      new int[] { y + height - 20, y + height - 15, y + height - 15, y + height - 20 },
-      4);
+        new int[] { x + 15, x + 15, x + 25, x + 25 },
+        new int[] { y + height - 20, y + height - 15, y + height - 15, y + height - 20 },
+        4);
     GraphicsUtil.drawCenteredArc(g, x + 20, y + height - 20, 5, 0, 180);
     g.drawPolyline(
-      new int[] { x + 20, x + 20, x + 175 },
-      new int[] { y + height - 25, y + height - 28, y + height - 28 },
-      3);
+        new int[] { x + 20, x + 20, x + 175 },
+        new int[] { y + height - 25, y + height - 28, y + height - 28 },
+        3);
 
     // A buffers
     for (int i = x + 30; i < x + 190; i += 20) {
       // input
       g.drawPolyline(
-        new int[] { i, i + 3, i + 10, i + 10 },
-        new int[] { y + height - AbstractTtlGate.PIN_HEIGHT, y + height - 10, y + height - 10, y + height - 17 },
-        4);
+          new int[] { i, i + 3, i + 10, i + 10 },
+          new int[] { y + height - AbstractTtlGate.PIN_HEIGHT, y + height - 10, y + height - 10, y + height - 17 },
+          4);
 
       // buffer
       g.drawPolyline(
-        new int[] { i + 6, i + 10, i + 14, i + 6 },
-        new int[] { y + height - 17, y + height - 23, y + height - 17, y + height - 17 },
-        4);
+          new int[] { i + 6, i + 10, i + 14, i + 6 },
+          new int[] { y + height - 17, y + height - 23, y + height - 17, y + height - 17 },
+          4);
       
       // enable
-      if ( i < x + 170 ) {
+      if (i < x + 170) {
         g.fillOval(i + 4, y + height - 29, 2, 2);
       }
       g.drawPolyline(
-        new int[] { i + 5, i + 5, i + 8 },
-        new int[] { y + height - 28, y + height - 21, y + height - 21 },
-        3);
+          new int[] { i + 5, i + 5, i + 8 },
+          new int[] { y + height - 28, y + height - 21, y + height - 21 },
+          3);
 
       // output
       g.drawPolyline(
-        new int[] { i + 10, i + 10, i + 16, i + 20 },
-        new int[] { y + height - 23, y + 16, y + 10, y + 10 },
-        4);
+          new int[] { i + 10, i + 10, i + 16, i + 20 },
+          new int[] { y + height - 23, y + 16, y + 10, y + 10 },
+          4);
       g.fillOval(i + 19, y + 9, 2, 2);
     }
 
     // B enable
     g.drawPolyline(
-      new int[] { x + 15, x + 15, x + 25, x + 25 },
-      new int[] { y + 20, y + 15, y + 15, y + 20 },
-      4);
+        new int[] { x + 15, x + 15, x + 25, x + 25 },
+        new int[] { y + 20, y + 15, y + 15, y + 20 },
+        4);
     GraphicsUtil.drawCenteredArc(g, x + 20, y + 20, 5, 180, 180);
     g.drawPolyline(
-      new int[] { x + 20, x + 20, x + 185 },
-      new int[] { y + 25, y + 28, y + 28 },
-      3);
+        new int[] { x + 20, x + 20, x + 185 },
+        new int[] { y + 25, y + 28, y + 28 },
+        3);
 
     // B buffers
     for (int i = x + 50; i < x + 210; i += 20) {
@@ -178,24 +178,24 @@ public class Ttl74245 extends AbstractTtlGate {
 
       // buffer
       g.drawPolyline(
-        new int[] { i + 4, i, i - 4, i + 4 },
-        new int[] { y + 17, y + 23, y + 17, y + 17 },
-        4);
+          new int[] { i + 4, i, i - 4, i + 4 },
+          new int[] { y + 17, y + 23, y + 17, y + 17 },
+          4);
       
       // enable
-      if ( i < x + 190 ) {
+      if (i < x + 190) {
         g.fillOval(i - 6, y + 27, 2, 2);
       }
       g.drawPolyline(
-        new int[] { i - 5, i - 5, i - 2 },
-        new int[] { y + 28, y + 21, y + 21 },
-        3);
+          new int[] { i - 5, i - 5, i - 2 },
+          new int[] { y + 28, y + 21, y + 21 },
+          3);
 
       // output
       g.drawPolyline(
-        new int[] { i, i, i - 6, i - 10 },
-        new int[] { y + 23, y + height - 16, y + height - 10, y + height - 10 },
-        4);
+          new int[] { i, i, i - 6, i - 10 },
+          new int[] { y + 23, y + height - 16, y + height - 10, y + height - 10 },
+          4);
       g.fillOval(i - 11, y + height - 11, 2, 2);
     }
   }
