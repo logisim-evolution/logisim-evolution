ae7a168b2f75a7c034a935e7b0299830563ee3fa
Dr.Ir.Ing. Theo Kluter <ktt1@bfh.ch>
2022-08-27 20:14:58 +0200

    First clean-up

--- src/main/java/com/cburch/logisim/analyze/data/EspressoCubeStruct.java
+++ src/main/java/com/cburch/logisim/analyze/data/EspressoCubeStruct.java
@@ -30,39 +30,44 @@ import java.util.List;
 import com.cburch.logisim.analyze.model.AnalyzerModel;
 
 public class EspressoCubeStruct {
+
+  // TODO: This code has not been optimized for performance, it just tries to implement espresso for logisim in java.
+  //       For sure a lot can be done here.
+  // Important: The original code is based on "unsigned int" for storage, as java does not know the unsigned version I
+  //            tried to adapt to int's, however I might have left some "bugs"
+  // Note: The (variable/function) names have been kept as close as possible to the original onces keeping in mind logisim
+  //       programming conventions.
+  public static final int BPI = 32; // number of bits in an Integer
+  public static final int LOG2_BPI = 5; // log2(BPI)
+  public static final int MaxInputMask = 0x03ff;
+  public static final int DISJOINT = 0x55555555;
   
-  public class EspressoCdataStruct {
-    List<Long> partZeros;
-    List<Long> varZeros;
-    List<Long> partsActive;
+  public class EspressoCdataStuct {
+    List<Integer> partZeros;
+    List<Integer> varZeros;
+    List<Integer> partActive;
     List<Boolean> isUnate;
     int varsActive;
     int varsUnate;
     int best;
   }
-      
+
   private int size;
   private int numberOfInputs;
   private int numberOfOutputs;
-  private List<Long> firstPart;
-  private List<Long> lastPart;
-  private List<Long> partSize;
-  private List<Long> firstWord;
-  private List<Long> lastWord;
-  private List<Long> binaryMask;
-  private List<List<Long>> varMask;
-  private List<List<Long>> temp;
-  private List<Long> fullSet;
-  private List<Long> emptySet;
+  private List<Integer> firstPart;
+  private List<Integer> lastPart;
+  private List<Integer> partSize;
+  private List<Integer> firstWord;
+  private List<Integer> lastWord;
+  private List<Integer> binaryMask;
+  private List<List<Integer>> varMask;
+  private List<Integer> fullSet;
+  private List<Integer> emptySet;
   private int inMask;
   private int inWord;
-  private List<Long> sparce;
-  private int outputIndex;
-  private EspressoCdataStruct cdata;
-
-  private final static int DISJOINT = 0x55555555;
-  private final static int CUBE_TEMP = 10;
-  private final static int BPI = 32;
+  private List<Integer> sparce;
+  private EspressoCdataStuct cData;
 
   public EspressoCubeStruct(int format, AnalyzerModel model) {
     final var truthTable = model.getTruthTable();
@@ -71,136 +76,119 @@ public class EspressoCubeStruct {
     setup();
   }
 
-  public List<Long> getTemp(int index) {
-    if ((index < 0) || (index >= temp.size())) return null;
-    return temp.get(index);
-  }
-
-  public int getSize() {
-    return size;
-  }
-
   private void setup() {
-    final var nrOfVars = numberOfInputs + 1;
-    outputIndex = numberOfInputs;
     size = 0;
-    firstPart = new ArrayList<>();
-    lastPart = new ArrayList<>();
-    partSize = new ArrayList<>();
-    firstWord = new ArrayList<>();
-    lastWord = new ArrayList<>();
-    for (var varIndex = 0; varIndex < nrOfVars; varIndex++) {
-      if (varIndex < numberOfInputs) partSize.add(2L);
-      firstPart.add((long) size);
-      firstWord.add(whichWord(size));
-      size += Math.abs(partSize.get(varIndex));
-      lastPart.add((long) size - 1L);
-      lastWord.add(whichWord(size - 1));
+    partSize = new ArrayList<>(numberOfInputs);
+    firstPart = new ArrayList<>(numberOfInputs);
+    lastPart = new ArrayList<>(numberOfInputs);
+    firstWord = new ArrayList<>(numberOfInputs);
+    lastWord = new ArrayList<>(numberOfInputs);
+    for (var input = 0; input < numberOfInputs; input++) {
+      partSize.set(input, 2); // this is a simplification for logisim, we do not take into account input busses 
+                                       // as the rest of logisim will take care of that
+      firstPart.set(input, size);
+      firstWord.set(input, whichWord(size));
+      size += partSize.get(input);
+      lastPart.set(input, size - 1 );
+      lastWord.set(input, whichWord(size - 1));
     }
-    partSize.add((long) numberOfOutputs);
-    varMask = new ArrayList<>();
-    sparce = new ArrayList<>();
+    varMask = new ArrayList<>(numberOfInputs);
+    sparce = new ArrayList<>(numberOfInputs);
     binaryMask = newCube();
-    for (var varIndex = 0; varIndex < nrOfVars; varIndex++) {
+    // mv-mask has been suppressed for logisim
+    for (var input = 0; input < numberOfInputs; input++) {
       final var mask = newCube();
-      varMask.add(mask);
-      for (var loop = firstPart.get(varIndex); loop < lastPart.get(varIndex); loop++) {
-        final var index = (int) whichWord(loop);
-        var value = mask.get(index);
-        value |= 1L << whichBit(loop);
-        mask.set(index, value);
-        if (varIndex < numberOfInputs) {
-          sparce.add(0L);
-          doSetOr(binaryMask, binaryMask, mask);
-        } else {
-          sparce.add(1L);
-        }
-      }      
+      varMask.set(input, mask);
+      for (var inputInfoMask = firstPart.get(input); inputInfoMask < lastPart.get(input); inputInfoMask++) {
+        setInsert(mask, inputInfoMask);
+      }
+      sparce.set(input, 0);
+      setOr(binaryMask, binaryMask, mask);
     }
-    inWord = lastWord.get(numberOfInputs - 1).intValue();
-    inMask = binaryMask.get(inWord).intValue() & DISJOINT;
-
-    temp = new ArrayList<>();
-    for (var index = 0; index < CUBE_TEMP; index++) temp.add(newCube());
-    emptySet = newCube();
+    inWord = lastWord.get(numberOfInputs - 1);
+    inMask = binaryMask.get(inWord) & DISJOINT;
     fullSet = setFill(newCube(), size);
-    cdata = new EspressoCdataStruct();
-    cdata.partZeros = allocLong(size);
-    cdata.varZeros = allocLong(nrOfVars);
-    cdata.partsActive = allocLong(nrOfVars);
-    cdata.isUnate = allocBool(nrOfVars);
+    emptySet = newCube();
+    cData = new EspressoCdataStuct();
+    cData.partZeros = new ArrayList<>(size);
+    cData.varZeros = new ArrayList<>(numberOfInputs);
+    cData.partActive = new ArrayList<>(numberOfInputs);
+    cData.isUnate = new ArrayList<>(numberOfInputs);
   }
 
-  public static long whichWord(long value) {
-    return (value >> 5L) + 1;
+  public static int whichWord(int element) {
+    // as we can expect for certain situations that we need more than 32 bits for information storage, this function
+    // returns in which of the 32-bits word the information is present.
+    // in-lining this would be more efficient but less readable
+    // Info: Logisim only supports 20 inputs and 256 outputs, hence element will never "over/underflow"/ be negative. Hence this check is suppressed.
+    //       the problem would occure if logisim supports > Integer.MAX_VALUE in-/outputs which is not likely to happen
+    return ((element >> LOG2_BPI) + 1);
   }
 
-  public static long whichBit(long value) {
-    return value & 31L;
+  public static int whichBit(int element) {
+    // only take the lower bits indexing the word
+    // in-lining this would be more efficient but less readable
+    // Info: Logisim only supports 20 inputs and 256 outputs, hence element will never "over/underflow"/ be negative. Hence this check is suppressed.
+    //       the problem would occure if logisim supports > Integer.MAX_VALUE in-/outputs which is not likely to happen
+    return element & (BPI-1);
   }
 
-  private static List<Long> allocLong(int size) {
-    final var res = new ArrayList<Long>();
-    for (var index = 0; index < size; index++) res.add(0L);
-    return res;
+  public static void setInsert(List<Integer> set, int entry) {
+    final var wordToTake = whichWord(entry);
+    var wordValue = set.get(wordToTake);
+    wordValue |= 1 << whichBit(entry);
   }
 
-  private static List<Boolean> allocBool(int size) {
-    final var res = new ArrayList<Boolean>();
-    for (var index = 0; index < size; index++) res.add(false);
-    return res;
+  public List<Integer> newCube() {
+    final var ret = new ArrayList<Integer>(setSize(size));
+    setClear(ret, size);
+    return ret;
   }
 
-  public static void doSetOr(List<Long> result, List<Long> set1, List<Long> set2) {
-    final var loopEnd = set1.get(0) & 0x03ffL;
-    var resultValue = result.get(0);
-    resultValue &= ~0x03ffL;
-    resultValue |= loopEnd;
-    result.set(0, resultValue);
-    for (var index = (int) loopEnd; index > 0; index--) {
-      final var val1 = set1.get(index);
-      final var val2 = set2.get(index);
-      result.set(index, val1 | val2);
+  public static List<Integer> setClear(List<Integer> data, int size) {
+    final var nrOfWords = loopInit(size);
+    data.set(0,nrOfWords); // indicates how many words hold the information
+    for (var word = 0 ; word < nrOfWords; word++) {
+      data.set(word + 1,0); // input 0 indicates the nr. of words. therefore + 1
+    }
+    return data;
+  }
+
+  public static List<Integer> setFill(List<Integer> data, int size) {
+    final var nrOfWords = loopInit(size);
+    data.set(0, nrOfWords);
+    final var lastWordRest = size % BPI;
+    for (var word = 0; word < nrOfWords - 1; word++) data.set(word + 1, 0xffffffff);
+    final var finalMask = lastWordRest == 31 ? 0xffffffff : (0x7fffffff >> (30 - lastWordRest)); // we have to take care of int versus unsigned int
+    data.set(nrOfWords, finalMask);
+    return data;
+  }
+
+  public static void setOr(List<Integer> result, List<Integer> array1, List<Integer> array2) {
+    final var nrOfWords = loop(array1);
+    putLoop(result, nrOfWords);
+    for (var infoIndex = 0; infoIndex < nrOfWords; infoIndex++) {
+      final var infoWord = infoIndex + 1;
+      result.set(infoWord, array1.get(infoWord) | array2.get(infoWord));
     }
   }
 
-  public static int loopInit(int size) {
-    return (size <= 32) ? 1 : (int) whichWord(size - 1);
+  private static int loop(List<Integer> set) {
+    return set.get(0) & MaxInputMask;
   }
 
-  public static List<Long> setFill(List<Long> res, int size) {
-    final var loopInit = loopInit(size);
-    res.set(0, (long) loopInit);
-    var value = 0xffffffffL;
-    value >>= (loopInit * BPI) - size;
-    res.set(loopInit, value);
-    for (var index = loopInit - 1; index > 0; index --) {
-      res.set(index, 0xffffffffL);
-    }
-    return res;
+  private static void putLoop(List<Integer> set, int value) {
+    var oldvalue = set.get(0);
+    oldvalue &= ~MaxInputMask;
+    oldvalue |= value;
+    set.set(0, oldvalue);
   }
 
-  public List<Long> setClear(List<Long> res, int size) {
-    res.clear();
-    final var loopInit = loopInit(size);
-    res.add((long) loopInit);
-    for (var i = loopInit; i > 0 ; i--) res.add(0L);
-    return res;
+  private static int setSize(int size) {
+    return (size <= BPI) ? 2 : whichWord(size - 1) + 1;
   }
 
-  public void setInsert(List<Long> res, int loc) {
-    final var index = (int) whichWord(loc);
-    var value = res.get(index);
-    value |= 1 << whichBit(loc);
-    res.set(index, value);
-  }
-
-  public void setCopy(List<Long> result, List<Long> source) {
-    result.clear();
-    result.addAll(source);
-  }
-
-  public List<Long> newCube() {
-    return setClear(new ArrayList<Long>(), size);
+  private static int loopInit(int size) {
+    return ((size <= BPI) ? 1 : whichWord(size));
   }
 }
