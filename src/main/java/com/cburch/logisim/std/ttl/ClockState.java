c37850146278b4634bfb503c75e65a56252c1aa8
Bunch of Bits <aloy@worldonline.nl>
2023-01-17 20:48:48 +0100

    Processed feedback comments

--- src/main/java/com/cburch/logisim/std/ttl/ClockState.java
+++ src/main/java/com/cburch/logisim/std/ttl/ClockState.java
@@ -28,6 +28,14 @@ class ClockState implements Cloneable {
     }
   }
 
+  /**
+   * Predicate which returns true if the clock has been triggered
+   *
+   * @param oldClock the previous state of the clock signal
+   * @param newClock the actual state of the clock signal
+   * @param trigger the trigger mode pos/neg edge or high/low level
+   * @return true if the clock was triggered, false otherwise
+   */
   private boolean isTriggered(Value oldClock, Value newClock, Object trigger) {
     if (trigger == null || trigger == StdAttr.TRIG_RISING) {
       return oldClock == Value.FALSE && newClock == Value.TRUE;
@@ -42,6 +50,14 @@ class ClockState implements Cloneable {
     }
   }
 
+  /**
+   * Update the clock state with the actual state of the clock signal
+   *
+   * @param newClock the actual state of the clock signal
+   * @param which which of the clock signals must be updated
+   * @param trigger the trigger mode pos/neg edge or high/low level
+   * @return true if the clock was triggered, false otherwise
+   */
   public boolean updateClock(Value newClock, int which, Object trigger) {
     if (lastClock.getWidth() <= which) {
       lastClock = lastClock.extendWidth(which + 1, Value.FALSE);
@@ -54,14 +70,39 @@ class ClockState implements Cloneable {
     return isTriggered(oldClock, newClock, trigger);
   }
 
+  /**
+   * Update the clock state with the actual state of the clock signal
+   * It is assumed that the clock is sensitive to the rising edge of
+   * the clock signal
+   *
+   * @param newClock the actual state of the clock signal
+   * @param which which of the clock signals must be updated
+   * @return true if the clock was triggered, false otherwise
+   */
   public boolean updateClock(Value newClock, int which) {
     return updateClock(newClock, which, StdAttr.TRIG_RISING);
   }
 
+  /**
+   * Update the clock state with the actual state of the clock signal
+   * It is assumed that clock[0] will be updated
+   *
+   * @param newClock the actual state of the clock signal
+   * @param trigger the trigger mode pos/neg edge or high/low level
+   * @return true if clock[0] was triggered, false otherwise
+   */
   public boolean updateClock(Value newClock, Object trigger) {
     return updateClock(newClock, 0, trigger);
   }
 
+  /**
+   * Update the clock state with the actual state of the clock signal
+   * It is assumed that clock[0] will be updated and that clock[0] is
+   * sensitive to the rising edge of the clock signal
+   *
+   * @param newClock the actual state of the clock signal
+   * @return true if a positive edge is detected on clock[0], false otherwise
+   */
   public boolean updateClock(Value newClock) {
     return updateClock(newClock, 0, StdAttr.TRIG_RISING);
   }
