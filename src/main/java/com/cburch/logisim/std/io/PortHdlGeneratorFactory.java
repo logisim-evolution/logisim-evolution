05b57769fb4ff4ca4633744aebac77e5201bae99
Dr.Ir.Ing. Theo Kluter <ktt1@bfh.ch>
2023-01-18 14:40:09 +0100

    Make linter happy

--- src/main/java/com/cburch/logisim/std/io/PortHdlGeneratorFactory.java
+++ src/main/java/com/cburch/logisim/std/io/PortHdlGeneratorFactory.java
@@ -61,11 +61,11 @@ public class PortHdlGeneratorFactory extends InlinedHdlGeneratorFactory {
     } else {
       // first we handle the input connections, and after that the output connections
       if (nrOfPins == 1) {
-          contents.add(
-              "{{assign}} {{1}}{{=}}{{2}}{{<}}{{3}}{{>}};",
-              Hdl.getNetName(componentInfo, 2, true, nets),
-              LOCAL_INOUT_BUBBLE_BUS_NAME,
-              endIndex);
+        contents.add(
+            "{{assign}} {{1}}{{=}}{{2}}{{<}}{{3}}{{>}};",
+            Hdl.getNetName(componentInfo, 2, true, nets),
+            LOCAL_INOUT_BUBBLE_BUS_NAME,
+            endIndex);
       } else {
         contents.add(
             "{{assign}} {{1}}{{=}}{{2}}{{<}}{{3}}{{4}}{{5}}{{>}};",
@@ -75,70 +75,70 @@ public class PortHdlGeneratorFactory extends InlinedHdlGeneratorFactory {
             Hdl.vectorLoopId(),
             startIndex);
       }
-        // simple case first, we have a single output enable
-        if (portType == PortIo.INOUTSE || nrOfPins == 1) {
-          if (Hdl.isVhdl()) {
-            if (nrOfPins == 1) {
-              contents
-                  .addVhdlKeywords()
-                  .add(
-                      "{{1}}({{2}}) <= {{3}} {{when}} {{4}} = '1' {{else}} ({{others}} => 'Z');",
-                      LOCAL_INOUT_BUBBLE_BUS_NAME,
-                      startIndex,
-                      Hdl.getNetName(componentInfo, 1, true, nets),
-                      Hdl.getNetName(componentInfo, 0, true, nets));
-            } else {
-              contents
-                  .addVhdlKeywords()
-                  .add(
-                      "{{1}}({{2}} {{downto}} {{3}}) <= {{4}} {{when}} {{5}} = '1' {{else}} ({{others}} => 'Z');",
-                      LOCAL_INOUT_BUBBLE_BUS_NAME,
-                      endIndex,
-                      startIndex,
-                      Hdl.getBusName(componentInfo, 1, nets),
-                      Hdl.getNetName(componentInfo, 0, true, nets));
-            }
+      // simple case first, we have a single output enable
+      if (portType == PortIo.INOUTSE || nrOfPins == 1) {
+        if (Hdl.isVhdl()) {
+          if (nrOfPins == 1) {
+            contents
+                .addVhdlKeywords()
+                .add(
+                    "{{1}}({{2}}) <= {{3}} {{when}} {{4}} = '1' {{else}} ({{others}} => 'Z');",
+                    LOCAL_INOUT_BUBBLE_BUS_NAME,
+                    startIndex,
+                    Hdl.getNetName(componentInfo, 1, true, nets),
+                    Hdl.getNetName(componentInfo, 0, true, nets));
           } else {
-            if (nrOfPins == 1) {
-              contents.add(
-                  "assign {{1}}[{{2}}] = ({{3}}) ? {{4}} : {{5}}'bZ;",
-                  LOCAL_INOUT_BUBBLE_BUS_NAME,
-                  startIndex,
-                  Hdl.getNetName(componentInfo, 0, true, nets),
-                  Hdl.getNetName(componentInfo, 1, true, nets),
-                  nrOfPins);
-            } else {
-              contents.add(
-                  "assign {{1}}[{{2}}:{{3}}] = ({{4}}) ? {{5}} : {{6}}'bZ;",
-                  LOCAL_INOUT_BUBBLE_BUS_NAME,
-                  endIndex,
-                  startIndex,
-                  Hdl.getNetName(componentInfo, 0, true, nets),
-                  Hdl.getBusName(componentInfo, 1, nets),
-                  nrOfPins);
-            }
+            contents
+                .addVhdlKeywords()
+                .add(
+                    "{{1}}({{2}} {{downto}} {{3}}) <= {{4}} {{when}} {{5}} = '1' {{else}} ({{others}} => 'Z');",
+                    LOCAL_INOUT_BUBBLE_BUS_NAME,
+                    endIndex,
+                    startIndex,
+                    Hdl.getBusName(componentInfo, 1, nets),
+                    Hdl.getNetName(componentInfo, 0, true, nets));
           }
         } else {
-          // we have to enumerate over each and every bit
-          for (var busBitIndex = 0; busBitIndex < nrOfPins; busBitIndex++) {
-            if (Hdl.isVhdl()) {
-              contents.addVhdlKeywords().add(
-                  "{{1}}({{2}}) <= {{3}} {{when}} {{4}} = '1' {{else}} 'Z';",
-                  LOCAL_INOUT_BUBBLE_BUS_NAME,
-                  startIndex + busBitIndex,
-                  Hdl.getBusEntryName(componentInfo, 1, true, busBitIndex, nets),
-                  Hdl.getBusEntryName(componentInfo, 0, true, busBitIndex, nets));
-            } else {
-              contents.add(
-                  "assign {{1}}[{{2}}] = ({{3}}) ? {{4}} : 1'bZ;",
-                  LOCAL_INOUT_BUBBLE_BUS_NAME,
-                  startIndex + busBitIndex,
-                  Hdl.getBusEntryName(componentInfo, 0, true, busBitIndex, nets),
-                  Hdl.getBusEntryName(componentInfo, 1, true, busBitIndex, nets));
-            }
+          if (nrOfPins == 1) {
+            contents.add(
+                "assign {{1}}[{{2}}] = ({{3}}) ? {{4}} : {{5}}'bZ;",
+                LOCAL_INOUT_BUBBLE_BUS_NAME,
+                startIndex,
+                Hdl.getNetName(componentInfo, 0, true, nets),
+                Hdl.getNetName(componentInfo, 1, true, nets),
+                nrOfPins);
+          } else {
+            contents.add(
+                "assign {{1}}[{{2}}:{{3}}] = ({{4}}) ? {{5}} : {{6}}'bZ;",
+                LOCAL_INOUT_BUBBLE_BUS_NAME,
+                endIndex,
+                startIndex,
+                Hdl.getNetName(componentInfo, 0, true, nets),
+                Hdl.getBusName(componentInfo, 1, nets),
+                nrOfPins);
+          }
+        }
+      } else {
+        // we have to enumerate over each and every bit
+        for (var busBitIndex = 0; busBitIndex < nrOfPins; busBitIndex++) {
+          if (Hdl.isVhdl()) {
+            contents.addVhdlKeywords().add(
+                "{{1}}({{2}}) <= {{3}} {{when}} {{4}} = '1' {{else}} 'Z';",
+                LOCAL_INOUT_BUBBLE_BUS_NAME,
+                startIndex + busBitIndex,
+                Hdl.getBusEntryName(componentInfo, 1, true, busBitIndex, nets),
+                Hdl.getBusEntryName(componentInfo, 0, true, busBitIndex, nets));
+          } else {
+            contents.add(
+                "assign {{1}}[{{2}}] = ({{3}}) ? {{4}} : 1'bZ;",
+                LOCAL_INOUT_BUBBLE_BUS_NAME,
+                startIndex + busBitIndex,
+                Hdl.getBusEntryName(componentInfo, 0, true, busBitIndex, nets),
+                Hdl.getBusEntryName(componentInfo, 1, true, busBitIndex, nets));
           }
         }
       }
+    }
     return contents;
   }
 }
