02c0aada16aaedc61c9b2875beaaaabbf8be8bce
David Hutchens <David.Hutchens@millersville.edu>
2023-04-18 07:50:36 -0400

    Remove outdated mockito-inline

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,6 @@ dependencies {
 
   testImplementation(platform("org.junit:junit-bom:5.9.2"))
   testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
-  testImplementation("org.mockito:mockito-inline:5.2.0")
   testImplementation("org.mockito:mockito-junit-jupiter:5.3.0")
 }
 
