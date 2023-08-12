273133103cacd1231b6be27a4fa73e2046a2daa5
hanyuan <2524395907@qq.com>
2023-08-12 16:36:56 +0800

    feat: add some interactions

--- src/main/java/com/cburch/logisim/util/JHotkeyInput.java
+++ src/main/java/com/cburch/logisim/util/JHotkeyInput.java
@@ -1,37 +1,99 @@
 package com.cburch.logisim.util;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.concurrent.Flow;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.Timer;
 import javax.swing.border.Border;
 
 public class JHotkeyInput extends JPanel {
-  private JButton resetButton = new JButton("❌");
-  private JTextField hotkeyInputField;
+  private final JButton resetButton = new JButton("❌");
+  private final JTextField hotkeyInputField;
+  private boolean preferredWidthSet=false;
   public JHotkeyInput(String text) {
-    super(new FlowLayout(FlowLayout.CENTER,0,0));
-    hotkeyInputField=new JTextField(text);
+    setLayout(new FlowLayout(FlowLayout.CENTER,0,0));
+    hotkeyInputField = new JTextField(text);
+    setBorder(hotkeyInputField.getBorder());
     hotkeyInputField.setHorizontalAlignment(SwingConstants.CENTER);
-
-    setBackground(hotkeyInputField.getBackground());
-    setBorder(BorderFactory.createCompoundBorder(
-        BorderFactory.createEmptyBorder(10,10,10,10),
-        hotkeyInputField.getBorder()
-    ));
-    resetButton.setBorder(BorderFactory.createEmptyBorder());
+    hotkeyInputField.setBackground(getBackground());
     hotkeyInputField.setBorder(BorderFactory.createEmptyBorder());
-//    setBackground(hotkeyInputField.getBackground());
-//    setBorder(BorderFactory.createEmptyBorder());
+    hotkeyInputField.addKeyListener(new HotkeyInputKeyListener(this));
+    hotkeyInputField.addFocusListener(new FocusListener() {
+      @Override
+      public void focusGained(FocusEvent e) {
+        resetButton.setVisible(true);
+      }
+
+      @Override
+      public void focusLost(FocusEvent e) {
+        resetButton.setVisible(false);
+      }
+    });
+    resetButton.setBorder(BorderFactory.createEmptyBorder());
+    resetButton.setVisible(false);
+    resetButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        resetButton.setVisible(false);
+      }
+    });
+    new Timer(200, e->{
+      if(!preferredWidthSet) {
+        Font font = resetButton.getFont();
+        resetButton.setFont(new Font(font.getFontName(), Font.PLAIN, 8));
+        resetButton.setPreferredSize(new Dimension(20, 20));
+        hotkeyInputField.setPreferredSize(new Dimension(getWidth() - 20, 30));
+        preferredWidthSet=true;
+      }
+    }).start();
+    setSize(new Dimension(150,30));
     add(hotkeyInputField);
     add(resetButton);
   }
 
+  public void updateLayout(){
+    preferredWidthSet=false;
+  }
+
   public void setText(String s){
     hotkeyInputField.setText(s);
+  }
+
+
+}
+
+class HotkeyInputKeyListener implements KeyListener {
+  private final JHotkeyInput hotkeyInput;
+  public HotkeyInputKeyListener(JHotkeyInput hotkeyInput){
+    this.hotkeyInput=hotkeyInput;
+  }
+  @Override
+  public void keyTyped(KeyEvent e) {
+    /* not-used */
+  }
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+
+    hotkeyInput.updateLayout();
+  }
+
+  @Override
+  public void keyReleased(KeyEvent e) {
+    /* not-used */
   }
 }
