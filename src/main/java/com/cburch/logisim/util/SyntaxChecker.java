f5d0ff56b6b154bb7d1729de1e87be533a0b2b02
figurantpp <figurantpp@protonmail.com>
2023-08-25 21:04:15 -0300

    Refactored StringBuilder and static fields in StaticChecker

--- src/main/java/com/cburch/logisim/util/SyntaxChecker.java
+++ src/main/java/com/cburch/logisim/util/SyntaxChecker.java
@@ -14,7 +14,6 @@ import static com.cburch.logisim.util.Strings.S;
 import com.cburch.logisim.fpga.designrulecheck.CorrectLabel;
 import com.cburch.logisim.fpga.hdlgenerator.HdlGeneratorFactory;
 import com.cburch.logisim.gui.generic.OptionPane;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class SyntaxChecker {
@@ -22,35 +21,36 @@ public final class SyntaxChecker {
   private static final Pattern variablePattern = Pattern.compile("^([a-zA-Z]+\\w*)");
   private static final Pattern forbiddenPattern = Pattern.compile("__");
 
-  private static Matcher forbiddenMatcher;
-  private static Matcher variableMatcher;
-
   private SyntaxChecker() {
     throw new IllegalStateException("Utility class. No instantiation allowed.");
   }
 
   public static String getErrorMessage(String val) {
     if (StringUtil.isNullOrEmpty(val)) return null;
-    variableMatcher = variablePattern.matcher(val);
-    forbiddenMatcher = forbiddenPattern.matcher(val);
+
+    final var variableMatcher = variablePattern.matcher(val);
+
+    final var forbiddenMatcher = forbiddenPattern.matcher(val);
+
     final var hdl = CorrectLabel.hdlCorrectLabel(val);
-    var message = "";
+
+    var messageBuilder = new StringBuilder();
+
     if (!variableMatcher.matches()) {
-      message = message.concat(S.get("variableInvalidCharacters"));
+      messageBuilder.append(S.get("variableInvalidCharacters"));
     }
     if (forbiddenMatcher.find()) {
-      message = message.concat(S.get("variableDoubleUnderscore"));
+      messageBuilder.append(S.get("variableDoubleUnderscore"));
     }
     if (hdl != null) {
-      message =
-          message.concat(
-              hdl.equals(HdlGeneratorFactory.VHDL)
-                  ? S.get("variableVHDLKeyword")
-                  : S.get("variableVerilogKeyword"));
+      messageBuilder.append(              hdl.equals(HdlGeneratorFactory.VHDL)
+          ? S.get("variableVHDLKeyword")
+          : S.get("variableVerilogKeyword"));
     }
     if (val.endsWith("_")) {
-      message = message.concat(S.get("variableEndsWithUndescore"));
+      messageBuilder.append(S.get("variableEndsWithUndescore"));
     }
+    var message = messageBuilder.toString();
     return (message.length() == 0) ? null : message;
   }
 
