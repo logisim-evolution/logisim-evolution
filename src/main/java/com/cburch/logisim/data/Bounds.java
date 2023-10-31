dd1f70bb705df14dce68c577137ef51975723254
figurantpp <figurantpp@protonmail.com>
2023-10-30 22:51:41 -0300

    FixMe: Started Bounds Documentation

--- src/main/java/com/cburch/logisim/data/Bounds.java
+++ src/main/java/com/cburch/logisim/data/Bounds.java
@@ -13,10 +13,19 @@ import com.cburch.logisim.util.Cache;
 import java.awt.Rectangle;
 
 /**
- * Represents an immutable rectangular bounding box. This is analogous to java.awt's <code>Rectangle
- * </code> class, except that objects of this type are immutable.
+ * Represents an immutable rectangular bounding box. This is analogous to the
+ * {@link java.awt.Rectangle Rectangle} class, except that objects of this type are immutable.
  */
 public class Bounds {
+
+  /**
+   * Returns a bounds object with the provided location and height.
+   * @param x The X coordinate of the object
+   * @param y The Y coordinate of the object
+   * @param wid The width of the object
+   * @param ht The height of the object
+   * @return A <code>Bounds</code> object with the provided fields.
+   */
   public static Bounds create(int x, int y, int wid, int ht) {
     final var hashCode = 13 * (31 * (31 * x + y) + wid) + ht;
     Object cached = cache.get(hashCode);
@@ -29,14 +38,25 @@ public class Bounds {
     return ret;
   }
 
+  /**
+   * Returns a bounds object from a <code>Rectangle</code> object.
+   * @return A <code>Bounds</code> object with x, y, width and height of the provided rectangle.
+   */
   public static Bounds create(java.awt.Rectangle rect) {
     return create(rect.x, rect.y, rect.width, rect.height);
   }
 
+  /**
+   * Returns a 1x1 bounds object with the given location.
+   * @return A 1x1 <code>Bounds</code> object with the X and Y from the provided location.
+   */
   public static Bounds create(Location pt) {
     return create(pt.getX(), pt.getY(), 1, 1);
   }
 
+  /**
+   * A <code>Bounds</code> object with 0 as its X, Y, width and height.
+   */
   public static final Bounds EMPTY_BOUNDS = new Bounds(0, 0, 0, 0);
 
   private static final Cache cache = new Cache();
@@ -53,6 +73,36 @@ public class Bounds {
     this.ht = ht;
   }
 
+  /**
+   * Determines the smallest rectangle that contains this rectangle and a given one.
+   * If one of the two rectangles is empty, returns the nonempty one, if present.
+   * If one of the two rectangles contains the other, returns the containing one.
+   * If none of the two rectangles contain each other, returns
+   * the smallest rectangle that contains both given rectangles.
+   *
+   * <p>
+   * Example:
+   * <pre>
+   * <code>
+   *
+   *    +-------+-+
+   *  /‾| this  | |
+   * R  +-------+ |
+   * e  |     +---+
+   * s  |     | o |
+   * u  |     | t |
+   * u  |     | h |
+   * l  |     | e |
+   * t  |     | r |
+   *  \_|     |   |
+   *    +-----+---+
+   * </code>
+   * </pre>
+   *
+   * @param bd The other rectangle to find a container with
+   * @return A bounds object with the least non-negative width and height that contains both
+   *         <code>this</code> and <code>bd</code>
+   */
   public Bounds add(Bounds bd) {
     if (this == EMPTY_BOUNDS) return bd;
     if (bd == EMPTY_BOUNDS) return this;
@@ -69,6 +119,16 @@ public class Bounds {
     }
   }
 
+  /**
+   * Determines the least rectangle that contains this and the given point.
+   * If this rectangle is empty, returns a 1x1 rectangle around the given point.
+   * Otherwise, returns a rectangle that contains both this and the provided point, with the least
+   * possible width and height.
+   *
+   * @param x The x coordinate of the given point
+   * @param y The x coordinate of the given point
+   * @return A bounds object with the least dimensions that contains this and the given point.
+   */
   public Bounds add(int x, int y) {
     if (this == EMPTY_BOUNDS) return Bounds.create(x, y, 1, 1);
     if (contains(x, y)) return this;
@@ -94,6 +154,16 @@ public class Bounds {
     return create(newX, newY, newWidth, newHeight);
   }
 
+  /**
+   * Determines the smallest rectangle that contains this rectangle and a given one.
+   * @param x The x coordinate of the given rectangle
+   * @param y The y coordinate of the given rectangle
+   * @param wid The width of the given rectangle
+   * @param ht The height of the given rectangle
+   * @return A bounds object with the least non-negative width and height that contains
+   *         both this and the given rectangle.
+   * @see Bounds#add(Bounds)
+   */
   public Bounds add(int x, int y, int wid, int ht) {
     if (this == EMPTY_BOUNDS) return Bounds.create(x, y, wid, ht);
     final var retX = Math.min(x, this.x);
@@ -107,10 +177,30 @@ public class Bounds {
     }
   }
 
+  /**
+   * Determines the least rectangle that contains this and the given point.
+   * If this rectangle is empty, returns a 1x1 rectangle around the given point.
+   * Otherwise, returns a rectangle that contains both this and the provided point, with the least
+   * possible width and height.
+   *
+   * @param p The given point
+   * @return A bounds object with the least dimensions that contains this and the given point.
+   * @see Bounds#add(int, int)
+   */
   public Bounds add(Location p) {
     return add(p.getX(), p.getY());
   }
 
+  /**
+   * Determines whether a given point is within a certain distance of
+   * a border of this rectangle.
+   * This method considers the right and bottom borders of this rectangle to be reduced by one
+   * in x and y respectively.
+   * @param px The X coordinate of the point
+   * @param py The Y coordinate of the point
+   * @param fudge The maximum distance from the border the given point might be.
+   * @return Whether the provided point is within <code>fudge</code> units from a border.
+   */
   public boolean borderContains(int px, int py, int fudge) {
     final var x1 = x + wid - 1;
     final var y1 = y + ht - 1;
@@ -125,18 +215,47 @@ public class Bounds {
     return false;
   }
 
+  /**
+   * Determines whether a given point is within a certain distance of
+   * a border of this rectangle.
+   * This method considers the right and bottom borders of this rectangle to be reduced by one
+   * in x and y respectively.
+   * @param p The point to check
+   * @param fudge The maximum distance from the border the given point might be.
+   * @return Whether the provided point is within <code>fudge</code> units from a border.
+   */
   public boolean borderContains(Location p, int fudge) {
     return borderContains(p.getX(), p.getY(), fudge);
   }
 
+  /**
+   * Determines whether this rectangle contains the given one.
+   * @param bd The other rectangle to check
+   * @return Whether the provided rectangle is contained by this one.
+   */
   public boolean contains(Bounds bd) {
     return contains(bd.x, bd.y, bd.wid, bd.ht);
   }
 
+  /**
+   * Determines whether this rectangle contains a given point.
+   * @param px The X coordinate of the point
+   * @param py The Y coordinate of the point
+   * @return Whether this rectangle contains the given point.
+   */
   public boolean contains(int px, int py) {
     return contains(px, py, 0);
   }
 
+  /**
+   * Determines whether this rectangle contains a given point, within a certain margin of error.
+   * If the least distance between the point and this rectangle is smaller or equal
+   * than the allowed error, returns true.
+   * @param px The X coordinate of the point
+   * @param py The Y coordinate of the point
+   * @param allowedError The maximum margin of error.
+   * @return Whether this rectangle contains the given point.
+   */
   public boolean contains(int px, int py, int allowedError) {
     return px >= x - allowedError
         && px < x + wid + allowedError
@@ -144,16 +263,37 @@ public class Bounds {
         && py < y + ht + allowedError;
   }
 
+  /**
+   * Determines whether this rectangle contains a given one.
+   * @param x The x coordinate of the given rectangle
+   * @param y The y coordinate of the given rectangle
+   * @param wid The width of the given rectangle
+   * @param ht The height of the given rectangle
+   * @return Whether the provided rectangle is contained by this one.
+   */
   public boolean contains(int x, int y, int wid, int ht) {
     final var othX = (wid <= 0 ? x : x + wid - 1);
     final var othY = (ht <= 0 ? y : y + ht - 1);
     return contains(x, y) && contains(othX, othY);
   }
 
+  /**
+   * Determines whether this rectangle contains a given point.
+   * @param p The point to check
+   * @return Whether this rectangle contains the given point.
+   */
   public boolean contains(Location p) {
     return contains(p.getX(), p.getY(), 0);
   }
 
+  /**
+   * Determines whether this rectangle contains a given point, within a certain margin of error.
+   * If the least distance between the point and this rectangle is smaller or equal
+   * than the allowed error, returns true.
+   * @param p The point to check
+   * @param allowedError The maximum margin of error.
+   * @return Whether this rectangle contains the given point.
+   */
   public boolean contains(Location p, int allowedError) {
     return contains(p.getX(), p.getY(), allowedError);
   }
@@ -165,32 +305,58 @@ public class Bounds {
            : false;
   }
 
+  /**
+   * Expands this rectangle, increasing its proportions in all directions.
+   *
+   * @param d The distance to increase
+   * @return A rectangle which equals this one, but <code>d</code> more units in the
+   *         north, south, east and west direction. The returned object's original position may not
+   *         equal this one's.
+   */
   public Bounds expand(int d) { // d pixels in each direction
     if (this == EMPTY_BOUNDS) return this;
     if (d == 0) return this;
     return create(x - d, y - d, wid + 2 * d, ht + 2 * d);
   }
 
+  /**
+   * @return The X coordinate of the center of this rectangle.
+   */
   public int getCenterX() {
     return (x + wid / 2);
   }
 
+  /**
+   * @return The Y coordinate of the center of this rectangle.
+   */
   public int getCenterY() {
     return (y + ht / 2);
   }
 
+  /**
+   * @return The height of this rectangle.
+   */
   public int getHeight() {
     return ht;
   }
 
+  /**
+   * @return The width of this rectangle.
+   */
   public int getWidth() {
     return wid;
   }
 
+  /**
+   * @return The X coordinate of this rectangle.
+   */
   public int getX() {
     return x;
   }
 
+  /**
+   * @return The Y coordinate of this rectangle.
+   */
   public int getY() {
     return y;
   }
@@ -203,6 +369,10 @@ public class Bounds {
     return ret;
   }
 
+  /**
+   * @return The rectangle that is the intersection between this rectangle and another one.
+   *         If no such rectangle exists, <code>EMPTY_BOUNDS</code> is returned.
+   */
   public Bounds intersect(Bounds other) {
     var x0 = this.x;
     var y0 = this.y;
