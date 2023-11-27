7aaea591162cf04a7210ea1d15df58b675177922
DHRUVIK MAHESHBHAI KAKADIYA <dh903632@dal.ca>
2023-11-27 15:12:45 -0400

    Extract Method

--- src/main/java/com/cburch/draw/shapes/RoundRectangle.java
+++ src/main/java/com/cburch/draw/shapes/RoundRectangle.java
@@ -76,6 +76,52 @@ public class RoundRectangle extends Rectangular {
     return S.get("shapeRoundRect");
   }
 
+//  @Override
+//  protected Location getRandomPoint(Bounds bds, Random rand) {
+//    if (getPaintType() != DrawAttr.PAINT_STROKE) {
+//      return super.getRandomPoint(bds, rand);
+//    }
+//
+//    final var w = getWidth();
+//    final var h = getHeight();
+//    final var r = radius;
+//    final var horz = Math.max(0, w - 2 * r); // length of horizontal segment
+//    final var vert = Math.max(0, h - 2 * r);
+//    final var len = 2 * horz + 2 * vert + 2 * Math.PI * r;
+//    var u = len * rand.nextDouble();
+//    var x = getX();
+//    var y = getY();
+//
+//    if (u < horz) {
+//      x += r + (int) u;
+//    } else if (u < 2 * horz) {
+//      x += r + (int) (u - horz);
+//      y += h;
+//    } else if (u < 2 * horz + vert) {
+//      y += r + (int) (u - 2 * horz);
+//    } else if (u < 2 * horz + 2 * vert) {
+//      x += w;
+//      y += (int) (u - 2 * w - h);
+//    } else {
+//      var rx = radius;
+//      var ry = radius;
+//      if (2 * rx > w) rx = w / 2;
+//      if (2 * ry > h) ry = h / 2;
+//      u = 2 * Math.PI * rand.nextDouble();
+//      final var dx = (int) Math.round(rx * Math.cos(u));
+//      final var dy = (int) Math.round(ry * Math.sin(u));
+//      x += (dx < 0) ? (r + dx) : (r + horz + dx);
+//      y += (dy < 0) ? (r + dy) : (r + vert + dy);
+//    }
+//
+//    final var d = getStrokeWidth();
+//    if (d > 1) {
+//      x += rand.nextInt(d) - d / 2;
+//      y += rand.nextInt(d) - d / 2;
+//    }
+//    return Location.create(x, y, false);
+//  }
+
   @Override
   protected Location getRandomPoint(Bounds bds, Random rand) {
     if (getPaintType() != DrawAttr.PAINT_STROKE) {
@@ -103,23 +149,32 @@ public class RoundRectangle extends Rectangular {
       x += w;
       y += (int) (u - 2 * w - h);
     } else {
-      var rx = radius;
-      var ry = radius;
-      if (2 * rx > w) rx = w / 2;
-      if (2 * ry > h) ry = h / 2;
-      u = 2 * Math.PI * rand.nextDouble();
-      final var dx = (int) Math.round(rx * Math.cos(u));
-      final var dy = (int) Math.round(ry * Math.sin(u));
-      x += (dx < 0) ? (r + dx) : (r + horz + dx);
-      y += (dy < 0) ? (r + dy) : (r + vert + dy);
+      randomizePointOnEllipse(x, y, r, horz, vert, w, h, rand);
     }
 
+    randomizePointWithStroke(x, y, rand);
+
+    return Location.create(x, y, false);
+  }
+
+  private void randomizePointOnEllipse(int x, int y, int r, int horz, int vert, int w, int h, Random rand) {
+    var rx = radius;
+    var ry = radius;
+    if (2 * rx > w) rx = w / 2;
+    if (2 * ry > h) ry = h / 2;
+    var u = 2 * Math.PI * rand.nextDouble();
+    final var dx = (int) Math.round(rx * Math.cos(u));
+    final var dy = (int) Math.round(ry * Math.sin(u));
+    x += (dx < 0) ? (r + dx) : (r + horz + dx);
+    y += (dy < 0) ? (r + dy) : (r + vert + dy);
+  }
+
+  private void randomizePointWithStroke(int x, int y, Random rand) {
     final var d = getStrokeWidth();
     if (d > 1) {
       x += rand.nextInt(d) - d / 2;
       y += rand.nextInt(d) - d / 2;
     }
-    return Location.create(x, y, false);
   }
 
   @Override
