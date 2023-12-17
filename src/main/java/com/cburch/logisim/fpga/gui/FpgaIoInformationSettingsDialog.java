9ec9a15a005ddc62b45d51566c2f6692e947ed2f
aplq <ethanbrinser@gmail.com>
2023-12-17 00:56:30 -0800

    Fixed window close button on some FPGA windows

--- src/main/java/com/cburch/logisim/fpga/gui/FpgaIoInformationSettingsDialog.java
+++ src/main/java/com/cburch/logisim/fpga/gui/FpgaIoInformationSettingsDialog.java
@@ -33,6 +33,8 @@ import java.awt.Frame;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.HashMap;
 import javax.swing.BorderFactory;
@@ -712,11 +714,17 @@ public class FpgaIoInformationSettingsDialog {
     CancelButton.addActionListener(actionListener);
     gbc.gridx = 0;
     contents.add(CancelButton, gbc);
+    // FIXME: Find better solution to handle window close button
+    selWindow.addWindowListener(new WindowAdapter() {
+      @Override
+      public void windowClosing(WindowEvent e) {
+        CancelButton.doClick();
+      }
+    });
     selWindow.add(new JScrollPane(contents));
     selWindow.pack();
     selWindow.setLocationRelativeTo(IOcomps.getParentFrame());
     selWindow.setModal(true);
-    selWindow.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
     selWindow.setAlwaysOnTop(true);
     abort = false;
     while (!abort) {
@@ -1087,6 +1095,13 @@ public class FpgaIoInformationSettingsDialog {
     gbc.gridy = 2;
     gbc.fill = GridBagConstraints.HORIZONTAL;
     selWindow.add(cancelButton, gbc);
+    // FIXME: Find better solution to handle window close button
+    selWindow.addWindowListener(new WindowAdapter() {
+      @Override
+      public void windowClosing(WindowEvent e) {
+        cancelButton.doClick();
+      }
+    });
 
     final var saveButton = new JButton(S.get("FpgaBoardDone"));
     saveButton.addActionListener(actionListener);
