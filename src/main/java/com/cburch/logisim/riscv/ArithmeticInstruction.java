da1298ca4d3f5c745cf4060792d8b3fb7a5f0b2b
serhii-iljin <serhii.iljin@gmail.com>
2024-06-15 22:24:47 +0100

    Testing of my update for R-type instruction fields in addi.

--- src/main/java/com/cburch/logisim/riscv/ArithmeticInstruction.java
+++ src/main/java/com/cburch/logisim/riscv/ArithmeticInstruction.java
@@ -7,7 +7,16 @@ public class ArithmeticInstruction {
         InstructionRegister ir = hartData.getIR();
         switch (ir.func3()) {
             case 0x0:   // addi rd,rs1,imm_I
+                System.out.println("opcode: " + ir.opcode());
+                System.out.println("destination register: " + ir.rd());
+                System.out.println("function: " + ir.func3());
+                System.out.println("source register 1: " + ir.rs1());
+                System.out.println("immediate value: " + ir.imm_I());
+                System.out.println("Test 0x300093 instruction: ");
+                System.out.println("BEFORE: x0 = " + hartData.getX(0) + ", x1 = " + hartData.getX(1));
                 hartData.setX(ir.rd(),hartData.getX(ir.rs1()) + ir.imm_I());
+                System.out.println("AFTER: x0 = " + hartData.getX(0) + ", x1 = " + hartData.getX(1));
+                System.out.println("Should be x1 = x0 + 3.");
                 break;
             case 0x4:   // xori rd,rs1,imm_I
                 hartData.setX(ir.rd(),hartData.getX(ir.rs1()) ^ ir.imm_I());
--- src/main/java/com/cburch/logisim/riscv/InstructionRegister.java
+++ src/main/java/com/cburch/logisim/riscv/InstructionRegister.java
@@ -20,15 +20,15 @@ public class InstructionRegister {
     // R-type instruction fields
     public int opcode() { return (int) (value & ((1 << 7)-1)); }        // value[6:0]
     public int rd() { return (int) ((value >> 7) & ((1 << 5)-1)); }     // value[11:7]
-    public int func3() { return (int) ((value >> 11) & ((1 << 3)-1)); } // value[14:12]
-    public int rs1() { return (int) ((value >> 14) & ((1 << 5)-1)); }   // value[19:15]
-    public int rs2() { return (int) ((value >> 19) & ((1 << 5)-1)); }   // value[24:20]
-    public int func7() { return (int) ((value >> 24) & ((1 << 7)-1)); } // value[31:25]
+    public int func3() { return (int) ((value >> 12) & ((1 << 3)-1)); } // value[14:12]
+    public int rs1() { return (int) ((value >> 15) & ((1 << 5)-1)); }   // value[19:15]
+    public int rs2() { return (int) ((value >> 20) & ((1 << 5)-1)); }   // value[24:20]
+    public int func7() { return (int) ((value >> 25) & ((1 << 7)-1)); } // value[31:25]
 
     // I-type additional fields
 
     public long imm_I() {  // sign-extended immediate field of I-type instruction
-        long res = ((value >> 19) & ((1 << 12) - 1)); // value[31:20], zero extended
+        long res = ((value >> 20) & ((1 << 12) - 1)); // value[31:20], zero extended
         res = (res ^ 0x800) - 0x800; // sign-extend 12 bit integer.
         return res;
     }
