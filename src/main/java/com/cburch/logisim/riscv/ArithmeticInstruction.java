4333ef731eb4e1f44b75df18dc25e7d49a9d84e5
NightXie <chayanne.mascarenhas@ucdconnect.ie>
2024-06-17 14:37:34 +0100

    Added implementation for R-Type Arithmetic Instructions

--- src/main/java/com/cburch/logisim/riscv/ArithmeticInstruction.java
+++ src/main/java/com/cburch/logisim/riscv/ArithmeticInstruction.java
@@ -38,4 +38,49 @@ public class ArithmeticInstruction {
                 break;
         }
     }
+
+    public static void executeRegister(rv32imData hartData){
+        InstructionRegister ir = hartData.getIR();
+        switch (ir.func3()) {
+            case 0x0:
+                switch (ir.func7()) {
+                    case 0x00:  // add rd,rs1,rs2
+                        hartData.setX(ir.rd(), hartData.getX(ir.rs1()) + hartData.getX(ir.rs2()));
+                        break;
+                    case 0x20:  // sub rd,rs1,rs2
+                        hartData.setX(ir.rd(), hartData.getX(ir.rs1()) - hartData.getX(ir.rs2()));
+                        break;
+                }
+                break;
+            case 0x1:   // sll rd,rs1,rs2
+                hartData.setX(ir.rd(), hartData.getX(ir.rs1()) << (hartData.getX(ir.rs2()) & 0x1f));
+                break;
+            case 0x2:   // slt rd,rs1,rs2
+                hartData.setX(ir.rd(), (hartData.getX(ir.rs1()) < hartData.getX(ir.rs2())) ? 1 : 0);
+                break;
+            case 0x3:   // sltu rd,rs1,rs2
+                hartData.setX(ir.rd(),
+                        (Long.compareUnsigned(hartData.getX(ir.rs1()),hartData.getX(ir.rs2())) < 0) ? 1 : 0);
+                break;
+            case 0x4:   // xor rd,rs1,rs2
+                hartData.setX(ir.rd(),hartData.getX(ir.rs1()) ^ hartData.getX(ir.rs2()));
+                break;
+            case 0x5:
+                switch (ir.func7()) {
+                    case 0x00:  // srl rd,rs1,rs2
+                        hartData.setX(ir.rd(), hartData.getX(ir.rs1()) >>> (hartData.getX(ir.rs2()) & 0x1f));
+                        break;
+                    case 0x20:  // sra rd,rs1,rs2
+                        hartData.setX(ir.rd(), hartData.getX(ir.rs1()) >> (hartData.getX(ir.rs2()) & 0x1f));
+                        break;
+                }
+                break;
+            case 0x6:   // or rd,rs1,rs2
+                hartData.setX(ir.rd(),hartData.getX(ir.rs1()) | hartData.getX(ir.rs2()));
+                break;
+            case 0x7:   // and rd,rs1,rs2
+                hartData.setX(ir.rd(),hartData.getX(ir.rs1()) & hartData.getX(ir.rs2()));
+                break;
+        }
+    }
 }
