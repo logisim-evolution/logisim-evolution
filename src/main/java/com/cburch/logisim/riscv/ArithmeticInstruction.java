8d781c439f2bee06bbd0c127f726f8357feb3019
NightXie <chayanne.mascarenhas@ucdconnect.ie>
2024-06-17 13:38:32 +0100

    Implemented remaining Arithmetic I-Type Instructions

--- src/main/java/com/cburch/logisim/riscv/ArithmeticInstruction.java
+++ src/main/java/com/cburch/logisim/riscv/ArithmeticInstruction.java
@@ -9,8 +9,33 @@ public class ArithmeticInstruction {
             case 0x0:   // addi rd,rs1,imm_I
                 hartData.setX(ir.rd(),hartData.getX(ir.rs1()) + ir.imm_I());
                 break;
+            case 0x1:   // slli rd,rs1,shamt_I
+                hartData.setX(ir.rd(), hartData.getX(ir.rs1()) << ir.rs2());
+                break;
+            case 0x2:   // slti rd,rs1,imm_I
+                hartData.setX(ir.rd(), (hartData.getX(ir.rs1()) < ir.imm_I()) ? 1 : 0);
+                break;
+            case 0x3:   // sltiu rd,rs1,imm_I //compare both rs1 & imm_I unsigned
+                hartData.setX(ir.rd(), (Long.compareUnsigned(hartData.getX(ir.rs1()),ir.imm_I()) < 0) ? 1 : 0);
+                break;
             case 0x4:   // xori rd,rs1,imm_I
                 hartData.setX(ir.rd(),hartData.getX(ir.rs1()) ^ ir.imm_I());
+                break;
+            case 0x5:
+                switch(ir.func7()) {
+                    case 0x00: // srli rd,rs1,shamt_I
+                        hartData.setX(ir.rd(), hartData.getX(ir.rs1()) >>> ir.rs2());
+                        break;
+                    case 0x20: // srai rd,rs1,shamt_I
+                        hartData.setX(ir.rd(), hartData.getX(ir.rs1()) >> ir.rs2());
+                        break;
+                }
+                break;
+            case 0x6:   // ori rd,rs1,imm_I
+                hartData.setX(ir.rd(), hartData.getX(ir.rs1()) | ir.imm_I());
+                break;
+            case 0x7:   // andi rd,rs1,imm_I
+                hartData.setX(ir.rd(), hartData.getX(ir.rs1()) & ir.imm_I());
                 break;
         }
     }
