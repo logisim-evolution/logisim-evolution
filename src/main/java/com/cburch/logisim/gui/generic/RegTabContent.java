09bc05fe6935c7b2862fc952d9bfed0378bcf0f2
redweasel <54547948+redweasel@users.noreply.github.com>
2025-03-30 15:36:07 +0200

    fix threading for the register tab

--- src/main/java/com/cburch/logisim/gui/generic/RegTabContent.java
+++ src/main/java/com/cburch/logisim/gui/generic/RegTabContent.java
@@ -30,13 +30,15 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.util.HashMap;
+import java.util.Map;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
 
 public class RegTabContent extends JScrollPane implements LocaleListener, Simulator.Listener {
   private static final long serialVersionUID = 1L;
-  private static final HashMap<String, Component> registers = new HashMap<>();
   private final JPanel panel = new JPanel(new GridBagLayout());
   private final GridBagConstraints gbc = new GridBagConstraints();
   private final Project proj;
@@ -58,81 +60,84 @@ public class RegTabContent extends JScrollPane implements LocaleListener, Simula
    * found, including their current value.
    */
   private void fillArray() {
-    registers.clear();
-    panel.removeAll();
+    HashMap<String, Component> registers = new HashMap<>();
     for (final var circ : proj.getLogisimFile().getCircuits()) {
-      getAllRegisters(circ);
+      getAllRegisters(circ, registers);
     }
     if (proj.getLogisimFile().getLibrary("prodis_v1.3") instanceof LoadedLibrary loadedLib) {
       if (loadedLib.getBase() instanceof LogisimFile lsFile) {
         for (final var circ : lsFile.getCircuits()) {
-          getAllRegisters(circ);
+          getAllRegisters(circ, registers);
         }
       }
     }
 
-    // FIXME: hardcoded strings
-    final var col1 = new MyLabel("Circuit", Font.ITALIC | Font.BOLD);
-    final var col2 = new MyLabel("Reg name", Font.BOLD);
-    final var col3 = new MyLabel("Value", Font.BOLD);
-
-    col1.setColor(Color.LIGHT_GRAY);
-    col2.setColor(Color.LIGHT_GRAY);
-    col3.setColor(Color.LIGHT_GRAY);
-
-    gbc.fill = GridBagConstraints.HORIZONTAL;
-    gbc.anchor = GridBagConstraints.FIRST_LINE_START;
-    gbc.ipady = 2;
-    gbc.weighty = 0;
-
-    var y = 0;
-    gbc.gridy = y;
-    gbc.gridx = 0;
-    gbc.weightx = 0.3;
-    panel.add(col1, gbc);
-    gbc.gridx++;
-    gbc.weightx = 0.5;
-    panel.add(col2, gbc);
-    gbc.gridx++;
-    gbc.weightx = 0.2;
-    panel.add(col3, gbc);
-    y++;
-
-    if (!registers.isEmpty()) {
-      final var keys = registers.keySet().stream().sorted(new AlphanumComparator()).toList();
-      for (final var key : keys) {
-        gbc.gridy = y;
-        gbc.gridx = 0;
-        final var circuitName = key.split("/")[0];
-        panel.add(new MyLabel(circuitName, Font.ITALIC, true), gbc);
-        gbc.gridx++;
-        final var registerName = key.split("/")[1];
-        panel.add(new MyLabel(registerName), gbc);
-        gbc.gridx++;
-        final var selReg = registers.get(key);
-        var mainCircState = proj.getCircuitState();
-        if (mainCircState == null) continue;
-        while (mainCircState.getParentState() != null) { // Get the main circuit
-          mainCircState = mainCircState.getParentState();
-        }
-        final var val = findVal(mainCircState, circuitName, selReg.getEnd(0).getLocation()); // Get Q port location
-
-        if (val != null) {
-          final var hexLabel = new MyLabel(val.toHexString());
-          hexLabel.setFont(new Font(Font.MONOSPACED, Font.PLAIN, hexLabel.getFont().getSize()));
-          panel.add(hexLabel, gbc);
-        } else {
-          panel.add(new MyLabel("-"), gbc);
-        }
-        y++;
-      }
-    }
-    gbc.weighty = 1;
-    gbc.gridy++;
-    gbc.gridx = 0;
-    gbc.weightx = 1;
-    panel.add(new MyLabel(""), gbc);
-    panel.validate();
+    // This is called from the Simulation Thread, so make sure it doesn't block.
+    SwingUtilities.invokeLater(
+        () -> {
+          panel.removeAll();
+      
+          // FIXME: hardcoded strings
+          final var col1 = new MyLabel("Circuit", Font.ITALIC | Font.BOLD);
+          final var col2 = new MyLabel("Reg name", Font.BOLD);
+          final var col3 = new MyLabel("Value", Font.BOLD);
+      
+          col1.setColor(Color.LIGHT_GRAY);
+          col2.setColor(Color.LIGHT_GRAY);
+          col3.setColor(Color.LIGHT_GRAY);
+      
+          gbc.fill = GridBagConstraints.HORIZONTAL;
+          gbc.anchor = GridBagConstraints.FIRST_LINE_START;
+          gbc.ipady = 2;
+          gbc.weighty = 0;
+      
+          var y = 0;
+          gbc.gridy = y;
+          gbc.gridx = 0;
+          gbc.weightx = 0.3;
+          panel.add(col1, gbc);
+          gbc.gridx++;
+          gbc.weightx = 0.5;
+          panel.add(col2, gbc);
+          gbc.gridx++;
+          gbc.weightx = 0.2;
+          panel.add(col3, gbc);
+          y++;
+        
+          final var keys = registers.keySet().stream().sorted(new AlphanumComparator()).toList();
+          for (final var key : keys) {
+            gbc.gridy = y;
+            gbc.gridx = 0;
+            final var circuitName = key.split("/")[0];
+            panel.add(new MyLabel(circuitName, Font.ITALIC, true), gbc);
+            gbc.gridx++;
+            final var registerName = key.split("/")[1];
+            panel.add(new MyLabel(registerName), gbc);
+            gbc.gridx++;
+            final var selReg = registers.get(key);
+            var mainCircState = proj.getCircuitState();
+            if (mainCircState == null) continue;
+            while (mainCircState.getParentState() != null) { // Get the main circuit
+              mainCircState = mainCircState.getParentState();
+            }
+            final var val = findVal(mainCircState, circuitName, selReg.getEnd(0).getLocation()); // Get Q port location
+    
+            if (val != null) {
+              final var hexLabel = new MyLabel(val.toHexString());
+              hexLabel.setFont(new Font(Font.MONOSPACED, Font.PLAIN, hexLabel.getFont().getSize()));
+              panel.add(hexLabel, gbc);
+            } else {
+              panel.add(new MyLabel("-"), gbc);
+            }
+            y++;
+          }
+          gbc.weighty = 1;
+          gbc.gridy++;
+          gbc.gridx = 0;
+          gbc.weightx = 1;
+          panel.add(new MyLabel(""), gbc);
+          panel.validate();
+        });
   }
 
   /**
@@ -167,7 +172,7 @@ public class RegTabContent extends JScrollPane implements LocaleListener, Simula
    *
    * @param circuit The circuit in which the registers are searched.
    */
-  private synchronized void getAllRegisters(Circuit circuit) {
+  private static void getAllRegisters(Circuit circuit, Map<String, Component> registers) {
     for (final var comp : circuit.getNonWires()) {
       if (comp.getFactory().getName().equals("Register")) {
         if (comp.getAttributeSet().getValue(Register.ATTR_SHOW_IN_TAB) && !comp.getAttributeSet().getValue(StdAttr.LABEL).equals("")) {
