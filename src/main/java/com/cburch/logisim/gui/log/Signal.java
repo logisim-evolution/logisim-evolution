0d20368e1ab262e0321d1288e0dddb738af84d77
seankcn <seankilcullen@hotmail.co.uk>
2025-05-01 23:40:49 +0100

    Fixed resizing issue causing https://github.com/logisim-evolution/logisim-evolution/issues/2005

--- src/main/java/com/cburch/logisim/gui/log/Signal.java
+++ src/main/java/com/cburch/logisim/gui/log/Signal.java
@@ -92,8 +92,8 @@ public class Signal {
       final var dur2 = new long[c + 1][];
       System.arraycopy(val, 0, val2, 0, c);
       System.arraycopy(dur, 0, dur2, 0, c);
-      val2[c + 1] = new Value[maxSize == 0 || (maxSize - cap) > CHUNK ? CHUNK : (maxSize - cap)];
-      dur2[c + 1] = new long[maxSize == 0 || (maxSize - cap) > CHUNK ? CHUNK : (maxSize - cap)];
+      val2[c] = new Value[maxSize == 0 || (maxSize - cap) > CHUNK ? CHUNK : (maxSize - cap)];
+      dur2[c] = new long[maxSize == 0 || (maxSize - cap) > CHUNK ? CHUNK : (maxSize - cap)];
       val = val2;
       dur = dur2;
       val[curSize / CHUNK][curSize % CHUNK] = v;
@@ -176,7 +176,7 @@ public class Signal {
     if (newMaxSize == maxSize) return;
     if (newMaxSize == 0 || newMaxSize > maxSize) {
       // growing
-      if (firstIndex != 0) retainOnly(0, curSize, newMaxSize); // keeps all data, but shifts it left
+      retainOnly(0, curSize, newMaxSize); // keeps all data, but shifts it left
     } else {
       // shrinking: newMaxSize < maxSize
       if (curSize <= newMaxSize) {
