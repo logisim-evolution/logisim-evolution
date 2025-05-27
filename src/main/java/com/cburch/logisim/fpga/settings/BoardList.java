8ff0a6251b6c68e32c300c32036e260973b54ff1
Theo Kluter <ktt1@bfh.ch>
2025-05-27 14:32:03 +0200

    Fixes the ZipSlip security detected.

--- src/main/java/com/cburch/logisim/fpga/settings/BoardList.java
+++ src/main/java/com/cburch/logisim/fpga/settings/BoardList.java
@@ -80,11 +80,10 @@ public class BoardList {
         try {
           // Check that fileName doesn't stray outside target directory: Zip Slip security test.
           // if getCanonicalFile throws IOException, then assume the worst.
-          if ((new File(dir, fileName)).getCanonicalFile().toPath().startsWith(dir.getCanonicalFile().toPath())) {
+          if ((new File(dir, fileName)).toPath().normalize().startsWith(dir.toPath())) {
             ret.add("url:" + fileName);
             continue;
           }
-
           zf.close();
         } catch (IOException e1) {
           // Do nothing since we are about to throw an Error anyway.
