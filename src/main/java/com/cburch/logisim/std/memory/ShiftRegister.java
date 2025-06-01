5b3bb061e1aa9927f03bc184a76008baa992100d
Theo Kluter <ktt1@bfh.ch>
2025-06-01 21:44:56 +0200

    Nr of bits can be a parameter, the problem was the number of stages and clasic or evolution version of the shiftreg.

--- src/main/java/com/cburch/logisim/std/memory/ShiftRegister.java
+++ src/main/java/com/cburch/logisim/std/memory/ShiftRegister.java
@@ -531,10 +531,9 @@ public class ShiftRegister extends InstanceFactory {
 
   @Override
   public String getHDLName(AttributeSet attrs) {
-    final var nrOfBits = attrs.getValue(StdAttr.WIDTH).getWidth();
     final var nrOfStages = attrs.getValue(ShiftRegister.ATTR_LENGTH);
     final var extension = (attrs.getValue(StdAttr.APPEARANCE) == StdAttr.APPEAR_CLASSIC) ? "Clasic" : "Evolution";
-    return String.format("SHIFTREG_%d_%d_%s", nrOfBits, nrOfStages, extension);
+    return String.format("SHIFTREG_%d_%s", nrOfBits, nrOfStages, extension);
   }
 
 }
