b4a2a21c9977339ea0e46d1cd08b12ca2e6ce44b
ITSMijaiL <ITSMijaiL@users.noreply.github.com>
2025-06-19 14:04:56 -0400

    Refactor goodAddr -> goodAddr1 for consistency

--- src/main/java/com/cburch/logisim/std/memory/DualPortRam.java
+++ src/main/java/com/cburch/logisim/std/memory/DualPortRam.java
@@ -244,7 +244,7 @@ public class DualPortRam extends Mem {
     long addr1 = addrValue1.toLongValue();
     long addr2 = addrValue2.toLongValue();
     long addrWrite = addrValueWrite.toLongValue();
-    final var goodAddr = addrValue1.isFullyDefined() && addr >= 0;
+    final var goodAddr1 = addrValue1.isFullyDefined() && addr1 >= 0;
     final var goodAddr2 = addrValue2.isFullyDefined() && addr >= 0;
     final var goodAddrWrite = addrValueWrite.isFullyDefined() && addr >= 0;
     if (goodAddrWrite && addrWrite != myState.getCurrent()) {
