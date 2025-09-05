cecf152116413f14efb45861065e622c33853bfe
Torsten Maehne <torsten.maehne@bfh.ch>
2025-09-05 16:42:11 +0200

    Remove unnecessary `@SuppressWarnings` annotations reported by the Java linter

--- src/main/java/com/cburch/logisim/circuit/SplitterAttributes.java
+++ src/main/java/com/cburch/logisim/circuit/SplitterAttributes.java
@@ -54,7 +54,6 @@ public class SplitterAttributes extends AbstractAttributeSet {
       return true;
     }
 
-    @SuppressWarnings({"rawtypes"})
     @Override
     public java.awt.Component getCellEditor(Integer value) {
       final var index = value;
