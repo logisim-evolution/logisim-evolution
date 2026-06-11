a07b2776362c41b53df70464f090f06f1524c0dd
johnb432 <58661205+johnb432@users.noreply.github.com>
2026-06-11 16:05:11 +0200

    Improve placement

--- src/main/java/com/cburch/logisim/gui/main/SelectionBase.java
+++ src/main/java/com/cburch/logisim/gui/main/SelectionBase.java
@@ -292,8 +292,8 @@ class SelectionBase {
       Bounds bds = computeBounds(comps);
       int centerX = bds.getX() + bds.getWidth() / 2;
       int centerY = bds.getY() + bds.getHeight() / 2;
-      int dx = ((int) (mousePos.x / zoomFactor - centerX)) / 10 * 10;
-      int dy = ((int) (mousePos.y / zoomFactor - centerY)) / 10 * 10;
+      int dx = ((int) Math.round((mousePos.x / zoomFactor - centerX) / 10)) * 10;
+      int dy = ((int) Math.round((mousePos.y / zoomFactor - centerY) / 10)) * 10;
       int newX = bds.getX() + dx;
       int newY = bds.getY() + dy;
       if (newX < 0) {
