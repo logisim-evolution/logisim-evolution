4ab4d6ad18c4987a013a0ca9e00a87852b71a8a3
hewzhew <hewzhew@bupt.edu.cn>
2026-08-21 19:44:42 +0800

    fpga: localize unconnected output warnings

--- src/test/java/com/cburch/logisim/circuit/CircuitHdlGeneratorFactoryTest.java
+++ src/test/java/com/cburch/logisim/circuit/CircuitHdlGeneratorFactoryTest.java
@@ -17,11 +17,17 @@ import static org.mockito.Mockito.when;
 import com.cburch.logisim.TestBase;
 import com.cburch.logisim.comp.Component;
 import com.cburch.logisim.data.AttributeSet;
+import com.cburch.logisim.fpga.designrulecheck.ConnectionEnd;
+import com.cburch.logisim.fpga.designrulecheck.ConnectionPoint;
+import com.cburch.logisim.fpga.designrulecheck.Net;
+import com.cburch.logisim.fpga.designrulecheck.Netlist;
 import com.cburch.logisim.fpga.designrulecheck.SimpleDrcContainer;
 import com.cburch.logisim.fpga.designrulecheck.netlistComponent;
 import com.cburch.logisim.fpga.gui.FpgaReportTabbedPane;
 import com.cburch.logisim.fpga.gui.Reporter;
+import com.cburch.logisim.fpga.hdlgenerator.Hdl;
 import com.cburch.logisim.instance.StdAttr;
+import com.cburch.logisim.util.LineBuffer;
 import com.cburch.logisim.util.LocaleManager;
 import java.util.Locale;
 import java.util.Map;
@@ -51,8 +57,112 @@ class CircuitHdlGeneratorFactoryTest extends TestBase {
     }
   }
 
+  @Test
+  void unconnectedOutputWarningsUseCurrentLocaleAndKeepGroundingBehavior() {
+    final var originalLocale = LocaleManager.getLocale();
+    try {
+      assertUnconnectedOutputWarnings(
+          Locale.ENGLISH,
+          "Found an unconnected output pin, tied the pin to ground!",
+          "Found an unconnected output bus pin, tied all the pin bits to ground!",
+          "Found an unconnected output bus pin, tied bit 0 to ground!");
+      assertUnconnectedOutputWarnings(
+          Locale.SIMPLIFIED_CHINESE,
+          "发现未连接的输出引脚；已将该引脚接地！",
+          "发现未连接的输出总线引脚；已将该总线引脚的所有位接地！",
+          "发现未连接的输出总线引脚；已将第 0 位接地！");
+    } finally {
+      LocaleManager.setLocale(originalLocale);
+    }
+  }
+
+  private void assertUnconnectedOutputWarnings(
+      Locale locale, String pinWarning, String busWarning, String busBitWarning) {
+    setLocaleAndReloadBundles(locale);
+    assertUnconnectedOutputPinWarning(pinWarning);
+    assertUnconnectedOutputBusWarning(busWarning);
+    assertPartiallyConnectedOutputBusWarning(busBitWarning);
+  }
+
+  private void assertUnconnectedOutputPinWarning(String expectedWarning) {
+    final var reporterGui = attachReporterGui();
+    final var component = mock(netlistComponent.class);
+    final var connection = mock(ConnectionEnd.class);
+    when(component.nrOfEnds()).thenReturn(1);
+    when(component.getEnd(0)).thenReturn(connection);
+    when(connection.getNrOfBits()).thenReturn(1);
+    when(connection.isOutputEnd()).thenReturn(false);
+    when(component.isEndConnected(0)).thenReturn(false);
+
+    assertEquals(
+        Map.of("output", Hdl.zeroBit()),
+        CircuitHdlGeneratorFactory.getSignalMap("output", component, 0, null));
+    assertSingleSevereWarning(reporterGui, expectedWarning);
+  }
+
+  private void assertUnconnectedOutputBusWarning(String expectedWarning) {
+    final var reporterGui = attachReporterGui();
+    final var component = mock(netlistComponent.class);
+    final var connection = mock(ConnectionEnd.class);
+    when(component.nrOfEnds()).thenReturn(1);
+    when(component.getEnd(0)).thenReturn(connection);
+    when(connection.getNrOfBits()).thenReturn(2);
+    when(connection.isOutputEnd()).thenReturn(false);
+    when(connection.get((byte) 0)).thenReturn(mock(ConnectionPoint.class));
+    when(connection.get((byte) 1)).thenReturn(mock(ConnectionPoint.class));
+
+    assertEquals(
+        Map.of("output", Hdl.getZeroVector(2, true)),
+        CircuitHdlGeneratorFactory.getSignalMap("output", component, 0, null));
+    assertSingleSevereWarning(reporterGui, expectedWarning);
+  }
+
+  private void assertPartiallyConnectedOutputBusWarning(String expectedWarning) {
+    final var reporterGui = attachReporterGui();
+    final var component = mock(netlistComponent.class);
+    final var connection = mock(ConnectionEnd.class);
+    final var disconnectedPoint = mock(ConnectionPoint.class);
+    final var connectedPoint = mock(ConnectionPoint.class);
+    final var connectedNet = mock(Net.class);
+    final var netlist = mock(Netlist.class);
+    when(component.nrOfEnds()).thenReturn(1);
+    when(component.getEnd(0)).thenReturn(connection);
+    when(connection.getNrOfBits()).thenReturn(2);
+    when(connection.isOutputEnd()).thenReturn(false);
+    when(connection.get((byte) 0)).thenReturn(disconnectedPoint);
+    when(connection.get((byte) 1)).thenReturn(connectedPoint);
+    when(connectedPoint.getParentNet()).thenReturn(connectedNet);
+    when(connectedNet.getBitWidth()).thenReturn(1);
+    when(netlist.isContinuesBus(component, 0)).thenReturn(false);
+    when(netlist.getNetId(connectedNet)).thenReturn(7);
+
+    final var signal =
+        CircuitHdlGeneratorFactory.getSignalMap("output", component, 0, netlist);
+
+    assertEquals(2, signal.size());
+    assertEquals(
+        Hdl.zeroBit(), signal.get(LineBuffer.formatHdl("{{1}}{{<}}{{2}}{{>}}", "output", 0)));
+    assertSingleSevereWarning(reporterGui, expectedWarning);
+  }
+
+  private FpgaReportTabbedPane attachReporterGui() {
+    final var reporterGui = mock(FpgaReportTabbedPane.class);
+    Reporter.report.setGuiLogger(reporterGui);
+    return reporterGui;
+  }
+
+  private void assertSingleSevereWarning(
+      FpgaReportTabbedPane reporterGui, String expectedWarning) {
+    final var warning = ArgumentCaptor.forClass(Object.class);
+    verify(reporterGui).addWarning(warning.capture());
+    assertEquals(expectedWarning, warning.getValue().toString());
+    assertEquals(
+        SimpleDrcContainer.LEVEL_SEVERE,
+        ((SimpleDrcContainer) warning.getValue()).getSeverity());
+  }
+
   private void assertInvalidSolderPointError(Locale locale, String expectedError) {
-    LocaleManager.setLocale(locale);
+    setLocaleAndReloadBundles(locale);
     final var reporterGui = mock(FpgaReportTabbedPane.class);
     Reporter.report.setGuiLogger(reporterGui);
     final var component = mock(netlistComponent.class);
@@ -71,5 +181,13 @@ class CircuitHdlGeneratorFactoryTest extends TestBase {
     assertEquals(expectedError, error.getValue().toString());
     assertEquals(
         SimpleDrcContainer.LEVEL_FATAL, ((SimpleDrcContainer) error.getValue()).getSeverity());
+  }
+
+  private void setLocaleAndReloadBundles(Locale locale) {
+    Hdl.zeroBit();
+    com.cburch.logisim.fpga.Strings.S.get("HdlUnconnectedOutputPinWarning");
+    LocaleManager.setLocale(
+        Locale.ENGLISH.equals(locale) ? Locale.SIMPLIFIED_CHINESE : Locale.ENGLISH);
+    LocaleManager.setLocale(locale);
   }
 }
