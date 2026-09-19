4090854598789bd57996be296f4bd271feee8c22
META-Xiao <makeyxiao@gmail.com>
2026-09-20 00:59:49 +0800

    test: remove display-dependent color picker tests

--- src/test/java/com/cburch/logisim/gui/prefs/TextColorPreferenceTest.java
+++ src/test/java/com/cburch/logisim/gui/prefs/TextColorPreferenceTest.java
@@ -10,27 +10,14 @@
 package com.cburch.logisim.gui.prefs;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.ArgumentMatchers.isNull;
-import static org.mockito.Mockito.mockStatic;
 
-import com.bric.colorpicker.ColorPicker;
-import com.cburch.logisim.gui.generic.OptionPane;
-import com.cburch.logisim.gui.generic.TextColorChooser;
 import com.cburch.logisim.prefs.AppPreferences;
-import com.cburch.logisim.std.base.Text;
-import com.cburch.logisim.tools.AddTool;
-import com.cburch.logisim.tools.TextTool;
 import com.cburch.logisim.util.ColorUtil;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.util.prefs.PreferenceChangeEvent;
 import javax.swing.SwingUtilities;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class TextColorPreferenceTest {
   private final String originalTheme = AppPreferences.LookAndFeel.get();
@@ -53,72 +40,6 @@ class TextColorPreferenceTest {
       assertEquals(originalPreference, AppPreferences.TEXT_TOOL_COLOR.get());
       AppPreferences.TEXT_TOOL_COLOR.removePropertyChangeListener(button);
       AppPreferences.LookAndFeel.removePropertyChangeListener(button);
-    });
-  }
-
-  @ParameterizedTest
-  @ValueSource(strings = {"TestLight", "TestDark"})
-  void confirmedPreferenceCreatesToolsWithOnlyTheLightColor(String theme) throws Exception {
-    SwingUtilities.invokeAndWait(() -> {
-      final var originalPreference = AppPreferences.TEXT_TOOL_COLOR.get();
-      final var button = new ColorChooserButton(null, AppPreferences.TEXT_TOOL_COLOR);
-      AppPreferences.LookAndFeel.set(theme);
-      final var stored = new Color(originalPreference).equals(Color.GRAY) ? Color.BLACK : Color.GRAY;
-      final var selected = ColorUtil.getThemeTextColor(stored);
-      try (final var dialogs = mockStatic(OptionPane.class)) {
-        dialogs.when(() -> OptionPane.showConfirmDialog(
-            isNull(), any(TextColorChooser.class), any(String.class),
-            eq(OptionPane.OK_CANCEL_OPTION), eq(OptionPane.PLAIN_MESSAGE)))
-            .thenAnswer(invocation -> {
-              final TextColorChooser chooser = invocation.getArgument(1);
-              ((ColorPicker) chooser.getComponent(0)).setColor(selected);
-              return OptionPane.OK_OPTION;
-            });
-        button.doClick();
-        refreshPreference();
-        assertEquals(stored.getRGB(), AppPreferences.TEXT_TOOL_COLOR.get());
-        assertEquals(stored, new TextTool().getAttributeSet().getValue(Text.ATTR_COLOR));
-        assertEquals(stored, new AddTool(Text.FACTORY).getAttributeSet().getValue(Text.ATTR_COLOR));
-        assertEquals(selected, swatch(button));
-      } finally {
-        AppPreferences.TEXT_TOOL_COLOR.set(originalPreference);
-        refreshPreference();
-        AppPreferences.TEXT_TOOL_COLOR.removePropertyChangeListener(button);
-        AppPreferences.LookAndFeel.removePropertyChangeListener(button);
-      }
-    });
-  }
-
-  private static void refreshPreference() {
-    final var monitor = AppPreferences.TEXT_TOOL_COLOR;
-    monitor.preferenceChange(
-        new PreferenceChangeEvent(AppPreferences.getPrefs(), monitor.getIdentifier(), ""));
-  }
-
-  @Test
-  void cancelDoesNotApplyTheEditedPreference() throws Exception {
-    SwingUtilities.invokeAndWait(() -> {
-      final var button = new ColorChooserButton(null, AppPreferences.TEXT_TOOL_COLOR);
-      final var originalPreference = AppPreferences.TEXT_TOOL_COLOR.get();
-      AppPreferences.LookAndFeel.set("TestDark");
-      try (final var dialogs = mockStatic(OptionPane.class)) {
-        dialogs.when(() -> OptionPane.showConfirmDialog(
-            isNull(), any(TextColorChooser.class), any(String.class),
-            eq(OptionPane.OK_CANCEL_OPTION), eq(OptionPane.PLAIN_MESSAGE)))
-            .thenAnswer(invocation -> {
-              final TextColorChooser chooser = invocation.getArgument(1);
-              ((ColorPicker) chooser.getComponent(0)).setColor(Color.RED);
-              return OptionPane.CANCEL_OPTION;
-            });
-        button.doClick();
-        dialogs.verify(() -> OptionPane.showConfirmDialog(
-            isNull(), any(TextColorChooser.class), any(String.class),
-            eq(OptionPane.OK_CANCEL_OPTION), eq(OptionPane.PLAIN_MESSAGE)));
-        assertEquals(originalPreference, AppPreferences.TEXT_TOOL_COLOR.get());
-      } finally {
-        AppPreferences.TEXT_TOOL_COLOR.removePropertyChangeListener(button);
-        AppPreferences.LookAndFeel.removePropertyChangeListener(button);
-      }
     });
   }
 
--- src/test/java/com/cburch/logisim/std/base/TextColorAttributeTest.java
+++ src/test/java/com/cburch/logisim/std/base/TextColorAttributeTest.java
@@ -10,22 +10,12 @@
 package com.cburch.logisim.std.base;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
-import com.bric.colorpicker.ColorPicker;
-import com.bric.colorpicker.colorslider.ColorSlider;
-import com.cburch.logisim.gui.generic.TextColorChooser;
 import com.cburch.logisim.instance.StdAttr;
 import com.cburch.logisim.prefs.AppPreferences;
-import com.cburch.logisim.util.ColorUtil;
-import java.awt.Color;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 class TextColorAttributeTest {
   private final String originalTheme = AppPreferences.LookAndFeel.get();
@@ -46,111 +36,5 @@ class TextColorAttributeTest {
     assertEquals("#0000ff80", Text.ATTR_COLOR.toStandardString(stored));
     assertEquals(stored, Text.ATTR_COLOR.parse("#0000ff80"));
     assertEquals("#0000ff80", StdAttr.LABEL_COLOR.toDisplayString(stored));
-  }
-
-  @ParameterizedTest
-  @CsvSource({
-    "TestLight, #ffffff",
-    "TestDark, #000000",
-    "TestDarcula, #000000",
-    "TestDark, #767676",
-    "TestDark, #0000ff"
-  })
-  void brightnessSliderTracksInitialColorAndSubsequentEdits(String theme, String stored)
-      throws Exception {
-    SwingUtilities.invokeAndWait(() -> {
-      AppPreferences.LookAndFeel.set(theme);
-      final var chooser = editor(Text.ATTR_COLOR.parse(stored));
-      final var picker = picker(chooser);
-      final var slider = java.util.Arrays.stream(picker.getComponents())
-          .filter(ColorSlider.class::isInstance)
-          .map(ColorSlider.class::cast)
-          .findFirst().orElseThrow();
-      assertEquals((int) (picker.getHSB()[2] * slider.getMaximum()), slider.getValue());
-      assertNull(chooser.getValue());
-
-      picker.setColor(Color.BLACK);
-      assertEquals(slider.getMinimum(), slider.getValue());
-      picker.setColor(Color.WHITE);
-      assertEquals(slider.getMaximum(), slider.getValue());
-
-      slider.setValue(slider.getMinimum());
-      assertEquals(Color.BLACK, picker.getColor());
-      slider.setValue(slider.getMaximum());
-      assertEquals(Color.WHITE, picker.getColor());
-    });
-  }
-
-  @Test
-  void openingClippedColorAndReturningToItDoesNotCreateAnEdit() throws Exception {
-    SwingUtilities.invokeAndWait(() -> {
-      AppPreferences.LookAndFeel.set("TestDark");
-      final var chooser = editor(Color.BLUE);
-      final var picker = picker(chooser);
-      final var displayed = Text.ATTR_COLOR.parse("#dadaff");
-      assertEquals(displayed, picker.getColor());
-      assertNull(chooser.getValue());
-      assertEquals(displayed, preview(chooser).getForeground());
-
-      picker.setColor(Color.RED);
-      picker.setColor(displayed);
-      assertNull(chooser.getValue());
-      assertEquals(displayed, preview(chooser).getForeground());
-    });
-  }
-
-  @Test
-  void lightSelectionIsStoredDirectly() throws Exception {
-    SwingUtilities.invokeAndWait(() -> {
-      AppPreferences.LookAndFeel.set("TestLight");
-      final var chooser = editor(Color.BLACK);
-      final var selected = Text.ATTR_COLOR.parse("#12345678");
-      picker(chooser).setColor(selected);
-
-      assertEquals(selected, chooser.getValue());
-      assertEquals(selected, preview(chooser).getForeground());
-    });
-  }
-
-  @Test
-  void darkSelectionStoresLightColorAndPreviewsItsActualRendering() throws Exception {
-    SwingUtilities.invokeAndWait(() -> {
-      AppPreferences.LookAndFeel.set("TestDark");
-      final var chooser = editor(Color.BLACK);
-      picker(chooser).setColor(Text.ATTR_COLOR.parse("#89898978"));
-
-      assertEquals(Text.ATTR_COLOR.parse("#76767678"), chooser.getValue());
-      assertEquals(Text.ATTR_COLOR.parse("#89898978"), preview(chooser).getForeground());
-
-      // Saturated blue cannot be reproduced exactly by the existing clipped mapping.
-      picker(chooser).setColor(Color.BLUE);
-      assertEquals(Text.ATTR_COLOR.parse("#dadaff"), chooser.getValue());
-      assertEquals(Text.ATTR_COLOR.parse("#202045"), preview(chooser).getForeground());
-      assertEquals(ColorUtil.getThemeTextColor(chooser.getValue()), preview(chooser).getForeground());
-    });
-  }
-
-  @Test
-  void opacityOnlyEditPreservesClippedSourceRgb() throws Exception {
-    SwingUtilities.invokeAndWait(() -> {
-      AppPreferences.LookAndFeel.set("TestDarcula");
-      final var chooser = editor(Color.BLUE);
-      picker(chooser).setColor(Text.ATTR_COLOR.parse("#dadaff78"));
-
-      assertEquals(Text.ATTR_COLOR.parse("#0000ff78"), chooser.getValue());
-      assertEquals(Text.ATTR_COLOR.parse("#dadaff78"), preview(chooser).getForeground());
-    });
-  }
-
-  private static TextColorChooser editor(Color stored) {
-    return (TextColorChooser) Text.ATTR_COLOR.getCellEditor(null, stored);
-  }
-
-  private static ColorPicker picker(TextColorChooser chooser) {
-    return (ColorPicker) chooser.getComponent(0);
-  }
-
-  private static JLabel preview(TextColorChooser chooser) {
-    return (JLabel) ((JPanel) chooser.getComponent(1)).getComponent(0);
   }
 }
